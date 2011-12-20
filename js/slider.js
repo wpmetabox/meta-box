4a1d4118efe05185d009d9b3b6498c279d828c66
franz-josef-kaiser <24-7@gmx.net>
2011-12-20 15:06:35 +0100

    added format and value display to slider
    Signed-off-by: franz-josef-kaiser <24-7@gmx.net>

--- js/slider.js
+++ js/slider.js
@@ -4,21 +4,33 @@ jQuery( document ).ready( function($)
 		 id		= null
 		,el		= null
 		,input	= null
+		,label	= null
+		,format	= null
 		,value	= null
+		,update	= null
 	;
 	$( '.rwmb-slider' ).each( function( i, val ) 
 	{
-		id		= jQuery( val ).attr( 'id' );
-		el		= jQuery( '#' + id );
-		input	= jQuery( '[name=' + id + ']' );
+		id		= $( val ).attr( 'id' );
+		el		= $( '#' + id );
+		input	= $( '[name=' + id + ']' );
+		label	= $( '[for=' + id + ']' );
+		format	= $( el ).attr( 'rel' );
+
+		$( label ).append( ': <span id="' + id + '-label"></span>' );
+		update	= $( '#' + id + '-label' );
 
 		if ( 'undefined' === $( input ).val() || null === typeof $( input ).val() )
 		{
 			$( input ).val( $( el ).slider( "values", 0 ) );
+			$( update ).val( "0" );
 		}
 		else
 		{
-			value = $( input ).val();
+			value = $( input ).val();console.log( value );
+			$( update ).text( value );
+			if ( 0 < format.length )
+				$( update ).append( ' ' + format );
 		}
 
 		el.slider(
@@ -27,7 +39,9 @@ jQuery( document ).ready( function($)
 			slide:	function( event, ui )
 			{
 				$( input ).val( ui.value );
+				$( update ).text( ui.value + ' ' + format );
 			}
 		} );
+		//console.log( $( label + ' span' ) );
 	});
 } );
