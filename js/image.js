9db4f94a34b60cb6f6eab3eaf14340d15aab6572
franz-josef-kaiser <24-7@gmx.net>
2012-01-06 12:42:47 +0100

    updated js code formatting to MB code standards

--- js/image.js
+++ js/image.js
@@ -1,25 +1,31 @@
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function($) 
+{
 	// Reorder images
-	$('.rwmb-images').each(function(){
-		var $this = $(this),
-			field_id = $this.parents('.rwmb-field').find('.field-id').val(),
-			data = {
-				action: 'rwmb_reorder_images',
-				_wpnonce: $('#nonce-reorder-images_' + field_id).val(),
-				post_id: $('.rwmb-post-id:first').val(),
-				field_id: field_id
-			};
-		$this.sortable({
-			placeholder: 'ui-state-highlight',
-			update: function (){
-				data.order = $this.sortable('serialize');
-
-				$.post(ajaxurl, data, function(r){
-					var res = wpAjax.parseAjaxResponse(r, 'ajax-response');
-					if (!res.errors)
-						alert(res.responses[0].data);
-				}, 'xml');
+	$( '.rwmb-images' ).each( function()
+	{
+		var 
+			$this		= $(this),
+			field_id	= $this.parents( '.rwmb-field' ).find( '.field-id' ).val(),
+			data		= {
+				action:		'rwmb_reorder_images',
+				_wpnonce:	$( '#nonce-reorder-images_' + field_id ).val(),
+				post_id:	$( '.rwmb-post-id:first' ).val(),
+				field_id:	field_id
 			}
-		});
-	});
-});
+		;
+		$this.sortable(
+		{
+			placeholder:	'ui-state-highlight',
+			update:			function (){
+				data.order = $this.sortable( 'serialize' );
+
+				$.post( ajaxurl, data, function( r )
+				{
+					var res = wpAjax.parseAjaxResponse( r, 'ajax-response' );
+					if ( ! res.errors )
+						alert( res.responses[0].data );
+				}, 'xml' );
+			}
+		} );
+	} );
+} );
