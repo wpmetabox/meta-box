44b5488c9bfe75e54bac91424b780b0887029575
franz-josef-kaiser <24-7@gmx.net>
2012-01-17 12:55:46 +0100

    prevent click event for clones: switch to jQuery default fn

--- js/clone.js
+++ js/clone.js
@@ -1,22 +1,27 @@
-jQuery(document).ready(function ($) {
+jQuery( document ).ready( function ($) 
+{
 	// Add more clones
-	$('.add-clone').click(function () {
-		var $input_last = $(this).parents('.rwmb-input').find('.rwmb-clone:last'),
-			$clone      = $input_last.clone(true);
+	$( '.add-clone' ).click( function ( event )
+	{
+		event.preventDefault();
+
+		var 
+			$input_last = $(this).parents('.rwmb-input').find('.rwmb-clone:last'),
+			$clone      = $input_last.clone(true)
+		;
 
 		$clone.insertAfter($input_last);
-
-		return false;
-	});
+	} );
 
 	// Remove clones
-	$('.rwmb-input').delegate('.remove-clone', 'click', function(){
-		var $this = $(this);
+	$( '.rwmb-input' ).delegate( '.remove-clone', 'click', function( event )
+	{
+		event.preventDefault();
+
+		var $this = $( this );
 
 		// Remove clone only if there're 2 or more of them
-		if ($this.parents('.rwmb-input').find('.rwmb-clone').length > 1)
-			$(this).parent().remove();
-
-		return false;
-	});
-});
+		if ( $this.parents( '.rwmb-input' ).find( '.rwmb-clone' ).length > 1 )
+			$( this ).parent().remove();
+	} );
+} );
