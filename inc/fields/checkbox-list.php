1df9d192df2786103c2d455292cf82c8b3177c6d
Anh Tran <rilwis@gmail.com>
2012-01-18 09:06:27 +0700

    Rename file, use "-" in filename

---
+++ inc/fields/checkbox-list.php
@@ -0,0 +1,33 @@
+<?php
+
+if ( ! class_exists( 'RWMB_Checkbox_List_Field' ) ) 
+{
+	class RWMB_Checkbox_List_Field 
+	{
+		/**
+		 * Get field HTML
+		 *
+		 * @param string $html
+		 * @param mixed  $meta
+		 * @param array  $field
+		 *
+		 * @return string
+		 */
+		static function html( $html, $meta, $field ) 
+		{
+			if ( ! is_array( $meta ) )
+				$meta = (array) $meta;
+
+			$html = array();
+
+			foreach ( $field['options'] as $key => $value ) 
+			{
+				$checked = checked( in_array( $key, $meta ), true, false );
+				$name = "name='{$field['field_name']}'";
+				$val     = " value='{$key}'";
+				$html[]  = "<input type='checkbox' class='rwmb-checkbox-list'{$name}{$val}{$checked} /> {$value}";
+			}
+			return implode( '<br />', $html );
+		}
+	}
+}
