0da7847b3108e72027237f054e515870159a2c22
franz-josef-kaiser <24-7@gmx.net>
2012-04-03 17:18:19 +0200

    update color.php/.js

--- js/color.js
+++ js/color.js
@@ -1,35 +1,24 @@
-/**
- * Update color picker element
- * Used for static & dynamic added elements (when clone)
- */
-function rwmb_update_color_picker()
-{
-	var $ = jQuery;
-	$( '.rwmb-color-picker' ).each( function()
-	{
-		var $this  = $( this ),
-			$input = $this.siblings( 'input.rwmb-color' );
-
-		// Make sure the value is displayed
-		if ( ! $input.val() )
-			$input.val( '#' );
-
-		$this.farbtastic( $input );
-	} );
-}
-
 jQuery( document ).ready( function($)
 {
-	$( '.rwmb-color' ).focus( function()
+	$( '.rwmb-color-picker' ).each( function()
+	{
+		var $this = $( this ), 
+			id    = $this.attr( 'rel' );
+
+		$this.farbtastic( '#' + id );
+	} );
+
+	$( '.rwmb-color' )
+	// On field in/focus
+	.focus( function()
 	{
 		$( this ).siblings( '.rwmb-color-picker' ).show();
 		return false;
 	} )
+	// On field out/blur
 	.blur( function() 
 	{
 		$( this ).siblings( '.rwmb-color-picker' ).hide();
 		return false;
 	} );
-
-	rwmb_update_color_picker();
 } );
