5aa825b8dfe4bea806a1bac8e659169bfaebada3
Anh Tran <rilwis@gmail.com>
2012-09-08 23:46:35 +0700

    Reformat JS code

--- js/thickbox-image.js
+++ js/thickbox-image.js
@@ -1,18 +1,18 @@
-jQuery( document ).ready( function($)
+jQuery( document ).ready( function( $ )
 {
-	// Reorder images
 	$( '.rwmb-thickbox-upload' ).click( function()
 	{
-		var $this = $(this),
+		var $this = $( this ),
 			$holder = $this.siblings( '.rwmb-images' ),
-			post_id = $('#post_ID').val(),
-			field_id = $this.attr('rel'),
+			post_id = $( '#post_ID' ).val(),
+			field_id = $this.attr( 'rel' ),
 			backup = window.send_to_editor;
-		window.send_to_editor = function(html) {
-			var $img = $('<div />').append(html).find('img'),
-				url = $img.attr('src'),
-				img_class = $img.attr('class'),
-				id = parseInt(img_class.replace(/\D/g, ''), 10);
+
+		window.send_to_editor = function( html ) {
+			var $img = $( '<div />' ).append( html ).find( 'img' ),
+				url = $img.attr( 'src' ),
+				img_class = $img.attr( 'class' ),
+				id = parseInt( img_class.replace( /\D/g, '' ), 10 );
 
 			html = '<li id="item_' + id + '">';
 			html += '<img src="' + url + '" />';
@@ -22,12 +22,12 @@ jQuery( document ).ready( function($)
 			html += '<input type="hidden" name="' + field_id + '[]" value="' + id + '" />';
 			html += '</li>';
 
-			$holder.append($(html)).removeClass('hidden');
+			$holder.append( $( html ) ).removeClass( 'hidden' );
 
 			tb_remove();
 			window.send_to_editor = backup;
 		}
-		tb_show('', 'media-upload.php?post_id=' + post_id + '&TB_iframe=true');
+		tb_show( '', 'media-upload.php?post_id=' + post_id + '&TB_iframe=true' );
 
 		return false;
 	} );
