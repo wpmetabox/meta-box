68b6881769c0911a3a85aab365b40417a768a792
Manny Fleurmond <funkatronic@gmail.com>
2012-11-01 09:25:31 -0500

    Added URL Field
    Url field automatically validates and sanitizes url's.

---
+++ inc/fields/url.php
@@ -0,0 +1,60 @@
+<?php
+// Prevent loading this file directly
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'RWMB_URL_Field' ) )
+{
+	class RWMB_URL_Field
+	{
+		/**
+		 * Get field HTML
+		 *
+		 * @param string $html
+		 * @param mixed  $meta
+		 * @param array  $field
+		 *
+		 * @return string
+		 */
+		static function html( $html, $meta, $field )
+		{
+			return sprintf(
+				'<input type="url" class="rwmb-url" name="%s" id="%s" value="%s" size="%s" />',
+				$field['field_name'],
+				$field['id'],
+				$meta,
+				$field['size']
+			);
+		}
+
+		/**
+		 * Normalize parameters for field
+		 *
+		 * @param array $field
+		 *
+		 * @return array
+		 */
+		static function normalize_field( $field )
+		{
+			$field = wp_parse_args( $field, array(
+				'size' => 30,
+			) );
+			return $field;
+		}
+		
+		/**
+		 * Sanitizes url
+		 *
+		 * @param $post_id
+		 * @param $field
+		 * @param $old
+		 * @param $new
+		 *
+		 * @return $new
+		 */
+		static function value( $new, $old, $post_id, $field) 
+		{
+			$new = esc_url($new);
+			return $new;
+		}
+	}
+}
