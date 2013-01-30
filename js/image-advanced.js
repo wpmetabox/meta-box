9874a3d448f0970ee0dbcc1c4a66fda89e23215c
Manny Fleurmond <funkatronic@gmail.com>
2013-01-30 08:49:27 -0600

    Added max file upload to image-advanced

--- js/image-advanced.js
+++ js/image-advanced.js
@@ -1,11 +1,15 @@
 jQuery( document ).ready( function( $ )
 {
 	var rwmb_media_frame,
-		rwmb_image_list;
+		rwmb_image_list,
+		max_file_uploads;
 	$( '.rwmb-image-advanced-upload' ).on( 'click', function(e) {
+		e.preventDefault();
+		
 		var upload_button = $(this);
 		rwmb_image_list = $(this).siblings( '.rwmb-images' );
-		e.preventDefault();
+		max_file_uploads = rwmb_image_list.data( 'max_file_uploads' );
+		
 		
 		// If the frame already exists, re-open it.
         if ( rwmb_media_frame ) {
@@ -14,26 +18,42 @@ jQuery( document ).ready( function( $ )
         }
 		//Create media frame
 		rwmb_media_frame = wp.media.frames.rwmb_media_frame = wp.media({
-
-            className: 'media-frame rwmb-media-frame',
-            frame: 'select',
-            multiple: true,
-            title: 'Upload Images',
-            library: {
-                type: 'image'
+            className	: 'media-frame rwmb-media-frame',
+            frame		: 'select',
+            multiple	: true,
+            title		: 'Upload Images',
+            library		: {
+            	type		:	'image'
             },
-            button: {
-                text:  'Select'
+            button		: {
+                text		:	'Select'
             }
         } );
+		
 		//Handle selection
 		rwmb_media_frame.on( 'select', function() {
 			//Get selections
-			var selection = rwmb_media_frame.state().get( 'selection' );
+			var selection = rwmb_media_frame.state().get( 'selection' ),
+				msg = 'You may only upload ' + max_file_uploads + ' file',
+				uploaded = rwmb_image_list.children().length,
+				total = uploaded + selection.length;
+			if ( max_file_uploads > 1 )
+				msg += 's';
+				
+			if( total > max_file_uploads )
+				alert( msg );
+				
+			if( total >= max_file_uploads )
+				upload_button.addClass( 'hidden' );
+			
 			selection.map( function( attachment, index ) {
-				console.log(index);
+				uploaded = rwmb_image_list.children().length;
+				if( uploaded + 1 + index > max_file_uploads ) {
+					return;
+				}
 				//Convert attachment to JSON			 
 				attachment = attachment.toJSON();
+				
 				//Attach attachment to field and get HTML
 				var data = {
 					action			: 'rwmb_attach_media',
@@ -49,14 +69,18 @@ jQuery( document ).ready( function( $ )
 					if ( res.errors )
 						alert( res.responses[0].errors[0].message );
 					else
-					{
 						rwmb_image_list.removeClass( 'hidden' ).prepend( res.responses[0].data );
-					}
 				}, 'xml' );
 			});
 		});
 		
+		
 		//Open
 		rwmb_media_frame.open();
+	} );
+	
+	$( '.rwmb-images' ).on( 'click', '.rwmb-delete-file', function()
+	{
+		$( this ).parents( '.rwmb-images' ).siblings( '.rwmb-image-advanced-upload' ).removeClass( 'hidden' );		
 	} );
 } );
