4650307791bf4a59d91d9841ff36a1b06dda15bc
Manny Fleurmond <funkatronic@gmail.com>
2013-03-27 13:04:54 -0500

    Color field now supports WordPress 3.5 color picker
    For WP 3.5+ installs, built color picker (currently Iris) will be used.
    Versions below will default to `farbtastic`

--- js/color.js
+++ js/color.js
@@ -3,8 +3,7 @@
  * Used for static & dynamic added elements (when clone)
  */
 jQuery( document ).ready( function( $ )
-{
-	
+{	
 	$( ':input.rwmb-color' ).each( rwmb_update_color_picker );
 	$( '.rwmb-input' ).on( 'clone', ':input.rwmb-color', rwmb_update_color_picker )
 	.on( 'focus', '.rwmb-color', function()
@@ -20,14 +19,24 @@ jQuery( document ).ready( function( $ )
 	function rwmb_update_color_picker()
 	{
 		var $this = $( this ),
+			$clone_container = $this.closest('.rwmb-clone'),
 			$color_picker = $this.siblings( '.rwmb-color-picker' );
-	
+		
 		// Make sure the value is displayed
 		if ( !$this.val() )
 			$this.val( '#' );
-	
-		$color_picker.farbtastic( $this );
-		
+			
+		if( typeof jQuery.wp === 'object' && typeof jQuery.wp.wpColorPicker === 'function' ){
+			if( $clone_container.length > 0 )
+			{
+				$this.appendTo( $clone_container ).siblings( 'div.wp-picker-container' ).remove();
+			}
+        	$this.wpColorPicker();
+		}
+		else {
+			//We use farbtastic if the WordPress color picker widget doesn't exist
+			$color_picker.farbtastic( $this );			
+		}			
 	}
 
 } );
