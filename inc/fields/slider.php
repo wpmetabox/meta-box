fdbaea77584ef9f738dc1f6d5fc3bb13ab7b4064
ZERGE <zerge.com@gmail.com>
2013-04-15 00:31:44 +0300

    Update slider.php

--- inc/fields/slider.php
+++ inc/fields/slider.php
@@ -16,6 +16,7 @@ if ( !class_exists( 'RWMB_Slider_Field' ) )
 			$url = RWMB_CSS_URL . 'jqueryui';
 			wp_enqueue_style( 'jquery-ui-core', "{$url}/jquery.ui.core.css", array(), '1.8.17' );
 			wp_enqueue_style( 'jquery-ui-theme', "{$url}/jquery.ui.theme.css", array(), '1.8.17' );
+			wp_enqueue_style( 'jquery-ui-slider', "{$url}/jquery.ui.slider.css", array(), '1.8.17' );
 			wp_enqueue_style( 'rwmb-slider', RWMB_CSS_URL . 'slider.css' );
 
 			wp_enqueue_script( 'rwmb-slider', RWMB_JS_URL . 'slider.js', array( 'jquery-ui-slider', 'jquery-ui-widget', 'jquery-ui-mouse', 'jquery-ui-core' ), RWMB_VER, true );
