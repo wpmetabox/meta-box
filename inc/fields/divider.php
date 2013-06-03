9a2da36d61e5d009b7358bf114680091e4319b57
Paul <pryley@gmail.com>
2013-06-03 15:41:35 +0200

    Add a new 'divider' field
    The 'divider' field has the following custom attribute:
    - header: heading text of the divider. Optional.
    Screenshot: http://i.imgur.com/3bC7hyC.png

---
+++ inc/fields/divider.php
@@ -0,0 +1,37 @@
+<?php
+// Prevent loading this file directly
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'RWMB_Divider_Field' ) )
+{
+	class RWMB_Divider_Field
+	{
+		/**
+		 * Enqueue scripts and styles
+		 *
+		 * @return void
+		 */
+		static function admin_enqueue_scripts()
+		{
+			wp_enqueue_style( 'rwmb-divider', RWMB_CSS_URL . 'divider.css', array(), RWMB_VER );
+		}
+		
+		/**
+		 * Get field HTML
+		 *
+		 * @param string $html
+		 * @param mixed  $meta
+		 * @param array  $field
+		 *
+		 * @return string
+		 */
+		static function html( $html, $meta, $field )
+		{
+			return sprintf(
+				'%s<hr />'
+				,!$field['heading'] ? '' : "<h4>{$field['heading']}</h4>"
+			);
+		}
+
+	}
+}
