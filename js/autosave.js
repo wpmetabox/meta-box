b27f81fac5d35a38aee47b9fb20e5f61b52499c6
pospi <pospi@spadgos.com>
2013-08-22 07:48:25 +0100

    fix bug in autocomplete handling javascript which prevents tag autocomplete working (possibly other items)

--- js/autosave.js
+++ js/autosave.js
@@ -2,7 +2,7 @@ jQuery( function( $ )
 {
 	$( document ).ajaxSend( function( e, xhr, s )
 	{
-		if ( -1 != s.data.indexOf( 'action=autosave' ) )
+		if ( typeof s.data != 'undefined' &&  -1 != s.data.indexOf( 'action=autosave' ) )
 		{
 			$( '.rwmb-meta-box').each( function()
 			{
