14b6f2ee64f3c6b86f72fac05b2ebaeb0bb94ba8
Vinicius <luiz.vinicius73@gmail.com>
2014-09-09 16:32:17 -0300

    fix.2 codesniffer

--- meta-box-loader.php
+++ meta-box-loader.php
@@ -2,14 +2,15 @@
 
 class RWMB_Loader
 {
- static function loader ($RWMB_URL, $RWMB_DIR){
-        define('RWMB_URL', $RWMB_URL);
-        define('RWMB_DIR', $RWMB_DIR);
+ static function loader( $RWMB_URL, $RWMB_DIR )
+ {
+	define( 'RWMB_URL', $RWMB_URL );
+	define( 'RWMB_DIR', $RWMB_DIR );
 
-        define('RWMB_JS_URL', trailingslashit(RWMB_URL . 'js'));
-        define('RWMB_CSS_URL', trailingslashit(RWMB_URL . 'css'));
+	define( 'RWMB_JS_URL', trailingslashit(RWMB_URL . 'js') );
+	define( 'RWMB_CSS_URL', trailingslashit(RWMB_URL . 'css') );
 
-        define('RWMB_INC_DIR', trailingslashit(RWMB_DIR . 'inc'));
-        define('RWMB_FIELDS_DIR', trailingslashit(RWMB_INC_DIR . 'fields'));
+	define( 'RWMB_INC_DIR', trailingslashit(RWMB_DIR . 'inc') );
+	define( 'RWMB_FIELDS_DIR', trailingslashit(RWMB_INC_DIR . 'fields') );
     }
 }
