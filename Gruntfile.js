1df6c6dfbe28ef921a07e903068df686f0cc1475
Tran Ngoc Tuan Anh <rilwis@gmail.com>
2014-09-13 10:01:48 +0700

    Add checktextdomain and makepot plugins for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,10 +12,10 @@ module.exports = function ( grunt )
 	grunt.initConfig( {
 
 		// Read config file
-		pkg         : grunt.file.readJSON( 'package.json' ),
+		pkg            : grunt.file.readJSON( 'package.json' ),
 
 		// Autoprefix CSS
-		autoprefixer: {
+		autoprefixer   : {
 			options: {
 				browsers: [
 					'last 2 versions',
@@ -28,7 +28,7 @@ module.exports = function ( grunt )
 		},
 
 		// Beautify CSS code
-		csscomb     : {
+		csscomb        : {
 			all: {
 				expand: true,
 				src   : allCssFiles
@@ -36,35 +36,85 @@ module.exports = function ( grunt )
 		},
 
 		// JSHint
-		jshint      : {
+		jshint         : {
 			options: {
 				reporter: require( 'jshint-stylish' ),
 				jshintrc: true // Auto search for .jshintrc files relative to the files being linted
 			},
 			all    : allJsFiles // Lint all JS files, except *.min.js (libraries)
-		}
+		},
+
+		// Check text domain
+		checktextdomain: {
+			release: {
+				options: {
+					text_domain: 'rwmb', // Specify allowed domain(s)
+					keywords   : [       // List keyword specifications
+						'__:1,2d',
+						'_e:1,2d',
+						'_x:1,2c,3d',
+						'esc_html__:1,2d',
+						'esc_html_e:1,2d',
+						'esc_html_x:1,2c,3d',
+						'esc_attr__:1,2d',
+						'esc_attr_e:1,2d',
+						'esc_attr_x:1,2c,3d',
+						'_ex:1,2c,3d',
+						'_n:1,2,4d',
+						'_nx:1,2,4c,5d',
+						'_n_noop:1,2,3d',
+						'_nx_noop:1,2,3c,4d'
+					]
+				},
+				files  : [{
+					expand: true,
+					src   : [
+						'**/*.php',
+						'!demo/*',
+						'!node_modules/**'
+					]
+				}]
+			}
+		},
 
 		// Update translation files
-		// makepot: {
-		// 	clone: {
-		// 		options: {
-		// 			cwd: 'deploy/new-themes/<%= pkg.name %>',
-		// 			potFilename: '',
-		// 			domainPath: '/languages',
-		// 			type: 'wp-theme',
-		// 			exclude: []
-		// 		}
-		// 	},
-		// 	theme: {
-		// 		options: {
-		// 			cwd: 'src',
-		// 			potFilename: '',
-		// 			domainPath: '/languages',
-		// 			type: 'wp-theme',
-		// 			exclude: []
-		// 		}
-		// 	}
-		// },
+		makepot        : {
+			release: {
+				options: {
+					domainPath : '/lang',       // Where to save the POT file.
+					potFilename: 'default.pot', // Name of the POT file.
+					type       : 'wp-plugin',   // Type of project (wp-plugin or wp-theme).
+					exclude    : ['demo/.*'],   // Don't translate demos, this is part of regex '|^' . $pattern . '$|',
+
+					processPot: function ( pot )
+					{
+						pot.headers['report-msgid-bugs-to'] = 'https://github.com/rilwis/meta-box\n';
+						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
+						pot.headers['x-poedit-basepath'] = '.';
+						pot.headers['x-poedit-language'] = 'English';
+						pot.headers['x-poedit-country'] = 'UNITED STATES';
+						pot.headers['x-poedit-sourcecharset'] = 'utf-8';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;_x:1,2c;_ex:1,2c;_n:1,2; _nx:1,2,4c;_n_noop:1,2;_nx_noop:1,2,3c;esc_attr__; esc_html__;esc_attr_e; esc_html_e;esc_attr_x:1,2c; esc_html_x:1,2c;';
+						pot.headers['x-poedit-bookmarks'] = '';
+						pot.headers['x-poedit-searchpath-0'] = '.';
+						pot.headers['x-textdomain-support'] = 'yes';
+						return pot;
+					}
+				}
+			}
+		}
+
+		// Deploy to wp.org
+		//wp_deploy      : {
+		//	deploy: {
+		//		options: {
+		//			plugin_slug: 'your-plugin-slug',
+		//			svn_user   : 'your-wp-repo-username',
+		//			build_dir  : 'build' //relative path to your build directory
+		//			assets_dir : 'wp-assets' //relative path to your assets directory (optional).
+		//		}
+		//	}
+		//}
 	} );
 
 
@@ -75,6 +125,10 @@ module.exports = function ( grunt )
 		'csscomb',
 
 		// JS tasks
-		'jshint'
+		'jshint',
+
+		// Translation
+		'checktextdomain',
+		'makepot'
 	] );
 };
