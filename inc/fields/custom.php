57ab7900f0cb6d140e486bbb242c5b1a13ecd97b
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-03-31 18:19:57 +0300

    added 'custom' filed which allows you to handle any custom html in you meta-box

---
+++ inc/fields/custom.php
@@ -0,0 +1,30 @@
+<?php
+// Prevent loading this file directly
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'RWMB_Text_Field' ) )
+{
+	class RWMB_Text_Field extends RWMB_Field
+	{
+		/**
+		 * Get field HTML
+		 *
+		 * @param mixed $meta
+		 * @param array $field
+		 *
+		 * @return string
+		 */
+		static function html( $meta, $field )
+		{
+			if( is_callable( $field[ 'callback' ] ) )
+			{
+				$html = call_user_func_array( $field[ 'callback' ], array( $meta, $field ) );
+			}
+			else
+			{
+				$html = apply_filters( 'rwmb_custom_field_html', '', $meta, $field );
+			}
+			return $html;
+		}
+	}
+}
