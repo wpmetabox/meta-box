f217a60ed9f1b3cd49f560765697f9c7a552c829
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-03-31 18:23:16 +0300

    improvements for 'custom' field

--- inc/fields/custom.php
+++ inc/fields/custom.php
@@ -16,6 +16,9 @@ if ( ! class_exists( 'RWMB_Text_Field' ) )
 		 */
 		static function html( $meta, $field )
 		{
+			if( empty( $field[ 'callback' ] ) )
+				return '';
+
 			if( is_callable( $field[ 'callback' ] ) )
 			{
 				$html = call_user_func_array( $field[ 'callback' ], array( $meta, $field ) );
