f02af12dc2ac2763983d073804a6cdbc57d29602
Anh <anh@fitwp.com>
2015-04-04 00:09:53 +0700

    Fix custom html field: coding standards, and return correct `$html` when no callback. Also remove filter, we have the filter already in RWMB_Field class. See http://metabox.io/docs/filters/#section-rwmb_field_type_html and http://metabox.io/docs/filters/#section-rwmb_field_id_html

--- inc/fields/custom-html.php
+++ inc/fields/custom-html.php
@@ -16,18 +16,10 @@ if ( ! class_exists( 'RWMB_Custom_Html_Field' ) )
 		 */
 		static function html( $meta, $field )
 		{
-			$html = !empty( $field[ 'std' ] ) ?  $field[ 'std' ] : '';
-
-			if( empty( $field[ 'callback' ] ) )
-				return '';
-
-			if( is_callable( $field[ 'callback' ] ) )
+			$html = ! empty( $field['std'] ) ? $field['std'] : '';
+			if ( ! empty( $field['callback'] ) && is_callable( $field['callback'] ) )
 			{
-				$html = call_user_func_array( $field[ 'callback' ], array( $meta, $field ) );
-			}
-			else
-			{
-				$html = apply_filters( 'rwmb_custom_field_html', $html, $meta, $field );
+				$html = call_user_func_array( $field['callback'], array( $meta, $field ) );
 			}
 			return $html;
 		}
