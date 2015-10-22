e9576743d1e38720373fed5296ee48724de077a1
Anh <anh@fitwp.com>
2015-10-22 14:52:26 +0700

    - Use `load_plugin_textdomain` to load language packs from translate.wordpress.org, and fallback to local translation - Change language file names to "$domain-$language" to match WordPress standard

--- inc/common.php
+++ inc/common.php
@@ -1,51 +1,46 @@
 <?php
+/**
+ * Handle common actions for the plugin: load text domain, add plugin links.
+ *
+ * @package Meta Box
+ */
+
 // Prevent loading this file directly
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( 'RWMB_Common' ) )
 {
 	/**
-	 * Common functions for the plugin
-	 * Independent from meta box/field classes
+	 * Class that handle actions for the plugin.
 	 */
 	class RWMB_Common
 	{
 		/**
-		 * Do actions when class is loaded
-		 *
+		 * Add actions when class is loaded.
 		 * @return void
 		 */
 		public static function on_load()
 		{
-			self::load_textdomain();
+			add_action( 'plugins_loaded', array( __CLASS__, 'load_textdomain' ) );
 
 			$plugin = 'meta-box/meta-box.php';
 			add_filter( "plugin_action_links_$plugin", array( __CLASS__, 'plugin_links' ) );
 		}
 
 		/**
-		 * Load plugin translation
-		 *
+		 * Load plugin translation.
 		 * @return void
 		 */
 		public static function load_textdomain()
 		{
-			// l18n translation files
-			$locale = get_locale();
-			$dir    = trailingslashit( RWMB_DIR . 'lang' );
-			$mofile = "{$dir}{$locale}.mo";
-
-			// In themes/plugins/mu-plugins directory
-			load_textdomain( 'meta-box', $mofile );
+			load_plugin_textdomain( 'meta-box', false, plugin_basename( RWMB_DIR ) . '/lang/' );
 		}
 
 		/**
-		 * Add links to Documentation and Extensions in plugin's list of action links
+		 * Add links to Documentation and Extensions in plugin's list of action links.
 		 *
 		 * @since 4.3.11
-		 *
 		 * @param array $links Array of action links
-		 *
 		 * @return array
 		 */
 		public static function plugin_links( $links )
@@ -54,7 +49,6 @@ if ( ! class_exists( 'RWMB_Common' ) )
 			$links[] = '<a href="http://metabox.io/plugins/">' . __( 'Extensions', 'meta-box' ) . '</a>';
 			return $links;
 		}
-
 	}
 
 	RWMB_Common::on_load();
