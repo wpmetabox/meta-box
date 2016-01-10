24d98afd56277b3677481aafd9772c7588e9b0c3
Manny Fleurmond <funkatronic@gmail.com>
2016-01-10 07:59:30 -0600

    checkbox-list now uses render_attributes

--- inc/fields/checkbox-list.php
+++ inc/fields/checkbox-list.php
@@ -16,19 +16,34 @@ class RWMB_Checkbox_List_Field extends RWMB_Multiple_Values_Field
 	{
 		$meta = (array) $meta;
 		$html = array();
-		$tpl  = '<label><input type="checkbox" class="rwmb-checkbox-list" name="%s" value="%s"%s> %s</label>';
+		$tpl  = '<label><input %s %s> %s</label>';
 
 		foreach ( $field['options'] as $value => $label )
 		{
+			$attributes = RWMB_Checkbox_Field::get_attributes( $field, $value );
 			$html[] = sprintf(
 				$tpl,
-				$field['field_name'],
-				$value,
+				self::render_attributes( $attributes ),
 				checked( in_array( $value, $meta ), 1, false ),
 				$label
 			);
 		}
 
 		return implode( '<br>', $html );
+	}
+	
+	/**
+	 * Normalize parameters for field
+	 *
+	 * @param array $field
+	 *
+	 * @return array
+	 */
+	static function normalize( $field )
+	{
+		$field = parent::normalize( $field );
+		$field = RWMB_Checkbox_Field::normalize( $field );
+
+		return $field;
 	}
 }
