cb4df42490126ab343bc930e46a5f84960a6f7a1
Manny Fleurmond <funkatronic@gmail.com>
2016-01-26 00:51:44 -0600

    Separate Walkers in separate files

---
+++ inc/walker.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Base Walker
+ * Walkers must inherit this class and overwrite methods with its own.
+ */
+abstract class RWMB_Walker extends Walker
+{
+	/**
+	 * Field data.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $field;
+
+	/**
+	 * Meta data.
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $meta = array();
+
+	function __construct( $db_fields, $field, $meta )
+	{
+		$this->db_fields = wp_parse_args( (array) $db_fields, array(
+			'parent' => '',
+			'id'     => '',
+			'label'  => '',
+		) );
+		$this->field     = $field;
+		$this->meta      = (array) $meta;
+	}
+}
