dae448966504631fed7fa670cec5b7f77acf9cc4
Manny Fleurmond <funkatronic@gmail.com>
2016-03-03 21:20:18 -0600

    Code refactoring for media fields

--- inc/fields/file-advanced.php
+++ inc/fields/file-advanced.php
@@ -5,6 +5,18 @@
 class RWMB_File_Advanced_Field extends RWMB_Media_Field
 {
 	/**
+	 * Add actions
+	 *
+	 * @return void
+	 */
+	static function add_actions()
+	{
+		parent::add_actions();
+		// Print attachment templates
+		add_action( 'print_media_templates', array( __CLASS__, 'print_templates' ) );
+	}
+
+	/**
 	 * Get the field value.
 	 * @param array $field
 	 * @param array $args
@@ -38,5 +50,56 @@ class RWMB_File_Advanced_Field extends RWMB_Media_Field
 	static function file_info( $file_id, $args = array() )
 	{
 		return RWMB_File_Field::file_info( $file_id, $args );
+	}
+
+	static function print_templates()
+	{
+		$i18n_remove         = apply_filters( 'rwmb_media_remove_string', _x( 'Remove', 'media', 'meta-box' ) );
+		$i18n_edit           = apply_filters( 'rwmb_media_edit_string', _x( 'Edit', 'media', 'meta-box' ) );
+		$i18n_view           = apply_filters( 'rwmb_media_view_string', _x( 'View', 'media', 'meta-box' ) );
+		$i18n_title          = _x( 'No Title', 'media', 'meta-box' );
+
+		?>
+		<script id="tmpl-rwmb-media-item" type="text/html">
+			<input type="hidden" name="{{{ data.fieldName }}}" value="{{{ data.id }}}" class="rwmb-media-input">
+			<div class="rwmb-media-preview">
+				<div class="rwmb-media-content">
+					<div class="centered">
+						<# if ( 'image' === data.type && data.sizes ) { #>
+							<# if ( data.sizes.thumbnail ) { #>
+								<img src="{{{ data.sizes.thumbnail.url }}}">
+							<# } else { #>
+								<img src="{{{ data.sizes.full.url }}}">
+							<# } #>
+						<# } else { #>
+							<# if ( data.image && data.image.src && data.image.src !== data.icon ) { #>
+								<img src="{{ data.image.src }}" />
+							<# } else { #>
+								<img src="{{ data.icon }}" />
+							<# } #>
+						<# } #>
+					</div>
+				</div>
+			</div>
+			<div class="rwmb-media-info">
+				<h4>
+					<a href="{{{ data.url }}}" target="_blank" title="<?php echo esc_attr( $i18n_view ); ?>">
+						<# if( data.title ) { #> {{{ data.title }}}
+							<# } else { #> <?php echo esc_attr( $i18n_title ); ?>
+						<# } #>
+					</a>
+				</h4>
+				<p>{{{ data.mime }}}</p>
+				<p>
+					<a class="rwmb-edit-media" title="<?php echo esc_attr( $i18n_edit ); ?>" href="{{{ data.editLink }}}" target="_blank">
+						<span class="dashicons dashicons-edit"></span><?php echo esc_attr( $i18n_edit ); ?>
+					</a>
+					<a href="#" class="rwmb-remove-media" title="<?php echo esc_attr( $i18n_remove ); ?>">
+						<span class="dashicons dashicons-no-alt"></span><?php echo esc_attr( $i18n_remove ); ?>
+					</a>
+				</p>
+			</div>
+		</script>
+		<?php
 	}
 }
--- inc/fields/media.php
+++ inc/fields/media.php
@@ -116,86 +116,10 @@ class RWMB_Media_Field extends RWMB_Field
 	static function print_templates()
 	{
 		$i18n_add            = apply_filters( 'rwmb_media_add_string', _x( '+ Add Media', 'media', 'meta-box' ) );
-		$i18n_remove         = apply_filters( 'rwmb_media_remove_string', _x( 'Remove', 'media', 'meta-box' ) );
-		$i18n_edit           = apply_filters( 'rwmb_media_edit_string', _x( 'Edit', 'media', 'meta-box' ) );
-		$i18n_view           = apply_filters( 'rwmb_media_view_string', _x( 'View', 'media', 'meta-box' ) );
 		$i18n_single_files   = apply_filters( 'rwmb_media_single_files_string', _x( ' file', 'media', 'meta-box' ) );
 		$i18n_multiple_files = apply_filters( 'rwmb_media_multiple_files_string', _x( ' files', 'media', 'meta-box' ) );
-		$i18n_title          = _x( 'No Title', 'media', 'meta-box' );
+
 		?>
-		<script id="tmpl-rwmb-media-item" type="text/html">
-			<input type="hidden" name="{{{ data.fieldName }}}" value="{{{ data.id }}}" class="rwmb-media-input">
-			<div class="rwmb-media-preview">
-				<div class="rwmb-media-content">
-					<div class="centered">
-						<# if ( 'image' === data.type && data.sizes ) { #>
-							<# if ( data.sizes.thumbnail ) { #>
-								<img src="{{{ data.sizes.thumbnail.url }}}">
-							<# } else { #>
-								<img src="{{{ data.sizes.full.url }}}">
-							<# } #>
-						<# } else { #>
-							<# if ( data.image && data.image.src && data.image.src !== data.icon ) { #>
-								<img src="{{ data.image.src }}" />
-							<# } else { #>
-								<img src="{{ data.icon }}" />
-							<# } #>
-						<# } #>
-					</div>
-				</div>
-			</div>
-			<div class="rwmb-media-info">
-				<h4>
-					<a href="{{{ data.url }}}" target="_blank" title="<?php echo esc_attr( $i18n_view ); ?>">
-						<# if( data.title ) { #> {{{ data.title }}}
-							<# } else { #> <?php echo esc_attr( $i18n_title ); ?>
-						<# } #>
-					</a>
-				</h4>
-				<p>{{{ data.mime }}}</p>
-				<p>
-					<a class="rwmb-edit-media" title="<?php echo esc_attr( $i18n_edit ); ?>" href="{{{ data.editLink }}}" target="_blank">
-						<span class="dashicons dashicons-edit"></span><?php echo esc_attr( $i18n_edit ); ?>
-					</a>
-					<a href="#" class="rwmb-remove-media" title="<?php echo esc_attr( $i18n_remove ); ?>">
-						<span class="dashicons dashicons-no-alt"></span><?php echo esc_attr( $i18n_remove ); ?>
-					</a>
-				</p>
-			</div>
-		</script>
-
-		<script id="tmpl-rwmb-image-item" type="text/html">
-			<input type="hidden" name="{{{ data.fieldName }}}" value="{{{ data.id }}}" class="rwmb-media-input">
-			<div class="rwmb-media-preview">
-				<div class="rwmb-media-content">
-					<div class="centered">
-						<# if ( 'image' === data.type && data.sizes ) { #>
-							<# if ( data.sizes.thumbnail ) { #>
-								<img src="{{{ data.sizes.thumbnail.url }}}">
-							<# } else { #>
-								<img src="{{{ data.sizes.full.url }}}">
-							<# } #>
-						<# } else { #>
-							<# if ( data.image && data.image.src && data.image.src !== data.icon ) { #>
-								<img src="{{ data.image.src }}" />
-							<# } else { #>
-								<img src="{{ data.icon }}" />
-							<# } #>
-						<# } #>
-					</div>
-				</div>
-			</div>
-			<div class="rwmb-overlay"></div>
-			<div class="rwmb-media-bar">
-				<a class="rwmb-edit-media" title="<?php echo esc_attr( $i18n_edit ); ?>" href="{{{ data.editLink }}}" target="_blank">
-					<span class="dashicons dashicons-edit"></span>
-				</a>
-				<a href="#" class="rwmb-remove-media" title="<?php echo esc_attr( $i18n_remove ); ?>">
-					<span class="dashicons dashicons-no-alt"></span>
-				</a>
-			</div>
-		</script>
-
 		<script id="tmpl-rwmb-add-media" type="text/html">
 			<?php echo $i18n_add; ?>
 		</script>
