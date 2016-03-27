8636948a10b558566e68ed0a1563706617c50fd2
Sam Najian <dev.samnajian@gmail.com>
2016-03-27 22:55:16 +0430

    add rtl styles

--- inc/meta-box.php
+++ inc/meta-box.php
@@ -99,6 +99,8 @@ class RW_Meta_Box
 			return;
 
 		wp_enqueue_style( 'rwmb', RWMB_CSS_URL . 'style.css', array(), RWMB_VER );
+		if( is_rtl() )
+			wp_enqueue_style( 'rwmb-rtl', RWMB_CSS_URL . 'style-rtl.css', array(), RWMB_VER );
 
 		// Load clone script conditionally
 		$fields = self::get_fields( $this->fields );
