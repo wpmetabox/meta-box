501d70673d1c260f0dfd646402f3bd96a77597cd
Anh <anh@fitwp.com>
2016-05-05 09:44:51 +0700

    Add fallback for autoload in PHP 5.2 in case it's disabled. #810

--- inc/loader.php
+++ inc/loader.php
@@ -18,7 +18,20 @@ class RWMB_Loader
 	public function __construct()
 	{
 		$this->constants();
+
+		/**
+		 * Register autoloader for plugin classes.
+		 * In PHP 5.3, SPL extension cannot be disabled and it's safe to use autoload.
+		 * However, hosting providers can disable it in PHP 5.2. In that case, we provide a fallback for autoload.
+		 * @link http://php.net/manual/en/spl.installation.php
+		 * @link https://github.com/rilwis/meta-box/issues/810
+		 */
 		spl_autoload_register( array( $this, 'autoload' ) );
+		if ( ! class_exists( 'RWMB_Core' ) )
+		{
+			$this->autoload_fallback();
+		}
+
 		$this->init();
 	}
 
@@ -110,6 +123,94 @@ class RWMB_Loader
 				require $dir . $file;
 				return;
 			}
+		}
+	}
+
+	/**
+	 * Fallback for autoload in PHP 5.2.
+	 */
+	public function autoload_fallback()
+	{
+		$files = array(
+			// Core
+			'core',
+			'helper',
+			'meta-box',
+			'validation',
+
+			// Walkers
+			'walkers/walker',
+			'walkers/select-walker',
+			'walkers/select-tree-walker',
+			'walkers/input-list-walker',
+
+			// Fields
+			'field',
+
+			'fields/multiple-values',
+			'fields/autocomplete',
+			'fields/text-list',
+
+			'fields/choice',
+
+			'fields/select',
+			'fields/select-advanced',
+			'fields/select-tree',
+
+			'fields/input-list',
+			'fields/radio',
+			'fields/checkbox-list',
+
+			'fields/object-choice',
+			'fields/post',
+			'fields/taxonomy',
+			'fields/taxonomy-advanced',
+			'fields/user',
+
+			'fields/input',
+
+			'fields/checkbox',
+			'fields/hidden',
+			'fields/number',
+			'fields/range',
+
+			'fields/text',
+			'fields/color',
+			'fields/datetime',
+			'fields/date',
+			'fields/time',
+			'fields/email',
+			'fields/fieldset-text',
+			'fields/key-value',
+			'fields/url',
+			'fields/oembed',
+			'fields/password',
+
+			'fields/media',
+			'fields/file-advanced',
+			'fields/file-upload',
+			'fields/image-advanced',
+			'fields/image-upload',
+			'fields/plupload-image',
+
+			'fields/file-input',
+			'fields/file',
+			'fields/image',
+			'fields/image-select',
+			'fields/thickbox-image',
+
+			'fields/button',
+			'fields/custom-html',
+			'fields/divider',
+			'fields/heading',
+			'fields/map',
+			'fields/slider',
+			'fields/textarea',
+			'fields/wysiwyg',
+		);
+		foreach ( $files as $file )
+		{
+			require RWMB_INC_DIR . "$file.php";
 		}
 	}
 
