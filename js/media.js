a43567eabee1630fdb51696faf12d0deead1254c
Manny Fleurmond <funkatronic@gmail.com>
2016-05-06 00:10:58 -0500

    Revert "Reformat code for media.js"
    This reverts commit 0db27b1168866dfd1ca2f8262ad478e44c75ec2f.

--- js/media.js
+++ js/media.js
@@ -6,13 +6,14 @@ jQuery( function ( $ )
 
 	var views = rwmb.views = rwmb.views || {},
 		MediaField, MediaList, MediaItem, MediaButton, MediaStatus;
+		rwmb.test = 'spoon';
 
 	MediaList = views.MediaList = Backbone.View.extend( {
-		tagName    : 'ul',
-		className  : 'rwmb-media-list',
+		tagName       	: 'ul',
+		className     	: 'rwmb-media-list',
 		addItemView: function ( item )
 		{
-			if ( !this.itemViews[item.cid] )
+			if( ! this.itemViews[item.cid] )
 			{
 				this.itemViews[item.cid] = new this.itemView( {
 					model     : item,
@@ -31,6 +32,7 @@ jQuery( function ( $ )
 
 		initialize: function ( options )
 		{
+			var that = this;
 			this.itemViews = {};
 			this.props = options.props;
 			this.itemView = options.itemView || MediaItem;
@@ -46,7 +48,7 @@ jQuery( function ( $ )
 				}
 			} );
 
-			// Sort media using sortable
+			//Sort media using sortable
 			this.initSort();
 
 			this.render();
@@ -67,18 +69,18 @@ jQuery( function ( $ )
 			this.props = new Backbone.Model( this.$el.data() );
 			this.props.set( 'fieldName', this.$input.attr( 'name' ) );
 
-			// Create collection
+			//Create collection
 			this.collection = new wp.media.model.Attachments();
 
-			// Create views
+			//Create views
 			this.createList();
-			this.createAddButton();
+			this.createAddButton()
 			this.createStatus();
 
-			// Render
+			//Render
 			this.render();
 
-			// Limit max files
+			//Limit max files
 			this.listenTo( this.collection, 'add', function ( item, collection )
 			{
 				var maxFiles = this.props.get( 'maxFiles' );
@@ -88,7 +90,7 @@ jQuery( function ( $ )
 				}
 			} );
 
-			// Load initial media
+			//Load initial media
 			if ( !_.isEmpty( this.values ) )
 			{
 				this.collection.props.set( {
@@ -102,18 +104,17 @@ jQuery( function ( $ )
 				this.collection.more();
 			}
 
-			// Listen for destroy event on input
-			this.$input.on( 'remove', function ()
-			{
-				if ( !that.props.get( 'forceDelete' ) )
-				{
-					return;
-				}
-				_.each( _.clone( that.collection.models ), function ( model )
-				{
-					model.destroy();
-				} );
-			} );
+			//Listen for destroy event on input
+			this.$input
+				.on( 'remove', function(){
+					if ( that.props.get( 'forceDelete' ) )
+					{
+						_.each( _.clone( that.collection.models ), function ( model )
+						{
+							model.destroy();
+						} );
+					}
+				} )
 		},
 
 		createList: function ()
@@ -133,7 +134,7 @@ jQuery( function ( $ )
 
 		render: function ()
 		{
-			// Empty then add parts
+			//Empty then add parts
 			this.$el
 				.empty()
 				.append(
@@ -151,7 +152,7 @@ jQuery( function ( $ )
 		initialize: function ( options )
 		{
 			this.props = options.props;
-			if ( !this.props.get( 'maxStatus' ) )
+			if( ! this.props.get( 'maxStatus' ) )
 				this.$el.hide();
 			this.listenTo( this.collection, 'add remove reset', this.render );
 			this.render();
@@ -168,15 +169,17 @@ jQuery( function ( $ )
 	} );
 
 	MediaButton = views.MediaButton = Backbone.View.extend( {
-		className : 'rwmb-add-media button',
-		tagName   : 'a',
-		events    : {
+		className: 'rwmb-add-media button',
+		tagName  : 'a',
+		events   : {
 			click: function ()
 			{
+				var models = this.collection.models;
+
 				// Destroy the previous collection frame.
 				if ( this._frame )
 				{
-					// this.stopListening( this._frame );
+					//this.stopListening( this._frame );
 					this._frame.dispose();
 				}
 
@@ -199,17 +202,19 @@ jQuery( function ( $ )
 				this._frame.open();
 			}
 		},
-		render    : function ()
+		render   : function ()
 		{
 			this.$el.text( i18nRwmbMedia.add );
 			return this;
 		},
+
 		initialize: function ( options )
 		{
 			this.props = options.props;
 			this.listenTo( this.collection, 'add remove reset', function ()
 			{
 				var maxFiles = this.props.get( 'maxFiles' );
+
 				if ( maxFiles > 0 )
 				{
 					this.$el.toggle( this.collection.length < maxFiles );
@@ -229,17 +234,17 @@ jQuery( function ( $ )
 			this.props = options.props;
 			this.render();
 			this.listenTo( this.model, 'destroy', function ( model )
-				{
-					this.collection.remove( this.model );
-				} )
-				.listenTo( this.model, 'change', function ()
-				{
-					this.render();
-				} );
+			{
+				this.collection.remove( this.model );
+			} )
+			.listenTo( this.model, 'change', function()
+			{
+				this.render();
+			});
 		},
 
 		events: {
-			'click .rwmb-remove-media': function ()
+			'click .rwmb-remove-media': function ( e )
 			{
 				this.collection.remove( this.model );
 				if ( this.props.get( 'forceDelete' ) )
