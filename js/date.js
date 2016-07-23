6e5c02a585af93a518958d63b76904e080086a6d
Manny Fleurmond <funkatronic@gmail.com>
2016-07-23 07:46:24 -0500

    Final fix for https://github.com/rilwis/meta-box/issues/923

--- js/date.js
+++ js/date.js
@@ -12,13 +12,13 @@ jQuery( function ( $ )
 			options = $this.data( 'options' ),
 			$inline = $this.siblings( '.rwmb-datetime-inline' ),
 			$timestamp = $this.siblings( '.rwmb-datetime-timestamp' ),
-			current = $this.val();
+			current = $this.val(),
+			$picker = $inline.length ? $inline : $this;
 
 		$this.siblings( '.ui-datepicker-append' ).remove(); // Remove appended text
 		if ( $timestamp.length )
 		{
-			var $picker = $inline.length ? $inline : $this;
-			options.onClose = function ()
+			options.onClose = options.onSelect = function ()
 			{
 				$timestamp.val( getTimestamp( $picker.datepicker( 'getDate' ) ) );
 			};
@@ -27,7 +27,13 @@ jQuery( function ( $ )
 		if ( $inline.length )
 		{
 			options.altField = '#' + $this.attr( 'id' );
-			options.onSelect = $timestamp.length ? options.onClose : '';
+			$this.on( 'keydown', _.debounce( function(){
+				$picker
+					.datepicker( 'setDate', $this.val() )
+					.find(".ui-datepicker-current-day")
+					.trigger("click");
+			}, 600 ) );
+
 			$inline
 				.removeClass( 'hasDatepicker' )
 				.empty()
--- js/datetime.js
+++ js/datetime.js
@@ -12,13 +12,13 @@ jQuery( function ( $ )
 			options = $this.data( 'options' ),
 			$inline = $this.siblings( '.rwmb-datetime-inline' ),
 			$timestamp = $this.siblings( '.rwmb-datetime-timestamp' ),
-			current = $this.val();
+			current = $this.val(),
+			$picker = $inline.length ? $inline : $this;
 
 		$this.siblings( '.ui-datepicker-append' ).remove(); // Remove appended text
 		if ( $timestamp.length )
 		{
-			var $picker = $inline.length ? $inline : $this;
-			options.onClose = function ()
+			options.onClose = options.onSelect = function ()
 			{
 				$timestamp.val( getTimestamp( $picker.datetimepicker( 'getDate' ) ) );
 			};
@@ -26,8 +26,14 @@ jQuery( function ( $ )
 
 		if ( $inline.length )
 		{
-			options.onSelect = $timestamp.length ? options.onClose : '';
 			options.altField = '#' + $this.attr( 'id' );
+			$this.on( 'keydown', _.debounce( function(){
+				$picker
+					.datepicker( 'setDate', $this.val() )
+					.find(".ui-datepicker-current-day")
+					.trigger("click");
+			}, 600 ) );
+			
 			$inline
 				.removeClass( 'hasDatepicker' )
 				.empty()
