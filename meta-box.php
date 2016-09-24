7639da8b3d70079c4be4f2d9a5919b1dfabc42c3
Anh <anh@fitwp.com>
2016-09-24 08:25:06 +0700

    Use dirname to include file. This makes Meta Box is safely included by Composer

--- meta-box.php
+++ meta-box.php
@@ -13,7 +13,7 @@
 
 if ( defined( 'ABSPATH' ) && ! defined( 'RWMB_VER' ) )
 {
-	require_once plugin_dir_path( __FILE__ ) . 'inc/loader.php';
+	require_once dirname( __FILE__ ) . '/inc/loader.php';
 	$loader = new RWMB_Loader;
 	$loader->init();
 }
