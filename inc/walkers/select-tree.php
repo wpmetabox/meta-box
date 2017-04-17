0ee3fedd9a7a3c230d295285910f77fddca413f7
Manny Fleurmond <funkatronic@gmail.com>
2017-04-16 23:44:45 -0500

    Simplify select_tree fix

--- inc/walkers/select-tree.php
+++ inc/walkers/select-tree.php
@@ -52,12 +52,10 @@ class RWMB_Walker_Select_Tree {
 		$children = array();
 
 		foreach ( $options as $option ) {
-			if( isset( $option->$parent ) ) {
-				$children[ $option->$parent ][] = $option;
-			} else {
-				$children[ 0 ][] = $option;
-			}
+			$index = isset( $option->$parent ) ? $option->$parent : 0;
+			$children[ $index ][] = $option;
 		}
+
 		$top_level = isset( $children[0] ) ? 0 : $options[0]->$parent;
 		return $this->display_level( $children, $top_level, true );
 	}
