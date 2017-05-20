ec018396c03d127f62614cdf7f609fe66482e1be
Truong Giang <truongwp@gmail.com>
2017-05-20 11:05:34 +0700

    Fixed text_list fields not saving correctly when edit not last field. Fixed #1117

--- inc/fields/text-list.php
+++ inc/fields/text-list.php
@@ -76,4 +76,17 @@ class RWMB_Text_List_Field extends RWMB_Multiple_Values_Field {
 		$output .= '</tr>';
 		return $output;
 	}
+
+	/**
+	 * Save meta value.
+	 *
+	 * @param mixed $new     The submitted meta value.
+	 * @param mixed $old     The existing meta value.
+	 * @param int   $post_id The post ID.
+	 * @param array $field   The field parameters.
+	 */
+	public static function save( $new, $old, $post_id, $field ) {
+		delete_post_meta( $post_id, $field['id'] );
+		parent::save( $new, array(), $post_id, $field );
+	}
 }
