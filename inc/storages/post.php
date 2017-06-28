df0406978d28aab776f6f2b00af76e95b097cd2d
Truong Giang <truongwp@gmail.com>
2017-06-28 11:53:23 +0700

    Add Base storage class. Post storage inherits from this class

--- inc/storages/post.php
+++ inc/storages/post.php
@@ -8,18 +8,7 @@
 /**
  * Class RWMB_Post_Storage
  */
-class RWMB_Post_Storage implements RWMB_Storage_Interface {
+class RWMB_Post_Storage extends RWMB_Base_Storage {
 
-	/**
-	 * Get value from storage.
-	 *
-	 * @param  int    $object_id Object id.
-	 * @param  string $name      Field name.
-	 * @param  array  $args      Custom arguments.
-	 * @return mixed
-	 */
-	public function get( $object_id, $name, $args = array() ) {
-		$single = ! empty( $args['single'] );
-		return get_post_meta( $object_id, $name, $single );
-	}
+	protected $object_type = 'post';
 }
