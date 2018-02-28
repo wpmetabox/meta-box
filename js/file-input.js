f36ca7efa4a731cecf869ea27520022be75cacca
Anh <anhtnt@elightup.com>
2018-02-28 17:45:12 +0700

    Fixed style for file input and background fields.
    Fixed file input field doesn't hide the Remove button when cloning.

--- js/file-input.js
+++ js/file-input.js
@@ -1,9 +1,10 @@
 jQuery( function ( $ ) {
 	'use strict';
 
-	var frame;
+	var frame,
+		$doc = $( document );
 
-	$( 'body' ).on( 'click', '.rwmb-file-input-select', function ( e ) {
+	$doc.on( 'click', '.rwmb-file-input-select', function ( e ) {
 		e.preventDefault();
 		var $el = $( this );
 
@@ -30,8 +31,13 @@ jQuery( function ( $ ) {
 	} );
 
 	// Clear selected images
-	$( 'body' ).on( 'click', '.rwmb-file-input-remove', function ( e ) {
+	$doc.on( 'click', '.rwmb-file-input-remove', function ( e ) {
 		e.preventDefault();
 		$( this ).addClass( 'hidden' ).siblings( 'input' ).val( '' );
+	} );
+
+	// Hide the Remove button when cloning
+	$doc.on( 'clone', '.rwmb-file_input', function () {
+		$( this ).siblings( '.rwmb-file-input-remove' ).addClass( 'hidden' );
 	} );
 } );
