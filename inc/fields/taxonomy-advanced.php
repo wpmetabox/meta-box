13735a6c4b1a1e47a32370bce0295e01948ff1dd
Anh <anhtnt@elightup.com>
2018-03-21 09:47:01 +0700

    Fixed cloning taxonomy_advanced field

--- inc/fields/taxonomy-advanced.php
+++ inc/fields/taxonomy-advanced.php
@@ -80,7 +80,7 @@ class RWMB_Taxonomy_Advanced_Field extends RWMB_Taxonomy_Field {
 		$meta = is_array( $meta ) ? array_map( 'wp_parse_id_list', $meta ) : wp_parse_id_list( $meta );
 		$meta = array_filter( $meta );
 
-		return $field['multiple'] ? $meta : reset( $meta );
+		return $meta;
 	}
 
 	/**
@@ -94,28 +94,37 @@ class RWMB_Taxonomy_Advanced_Field extends RWMB_Taxonomy_Field {
 	 * @return array List of post term objects.
 	 */
 	public static function get_value( $field, $args = array(), $post_id = null ) {
-		if ( ! $post_id ) {
-			$post_id = get_the_ID();
-		}
-
-		// Get raw meta value in the database, no escape.
-		$value = self::call( $field, 'raw_meta', $post_id, $args );
-		if ( empty( $value ) ) {
-			return null;
-		}
-
-		// Allow to pass more arguments to "get_terms".
-		$args  = wp_parse_args( array(
-			'include'    => $value,
-			'hide_empty' => false,
-		), $args );
-		$value = get_terms( $field['taxonomy'], $args );
-
-		// Get single value if necessary.
-		if ( ! $field['clone'] && ! $field['multiple'] ) {
-			$value = reset( $value );
+		$value = RWMB_Field::get_value( $field, $args, $post_id );
+		if ( ! $field['clone'] ) {
+			$value = self::call( 'terms_info', $field, $value, $args );
+		} else {
+			$return = array();
+			foreach ( $value as $subvalue ) {
+				$return[] = self::call( 'terms_info', $field, $subvalue, $args );
+			}
+			$value = $return;
 		}
 
 		return $value;
+	}
+
+	/**
+	 * Get terms information.
+	 *
+	 * @param array  $field    Field parameters.
+	 * @param string $term_ids Term IDs, in CSV format.
+	 * @param array  $args     Additional arguments (for image size).
+	 *
+	 * @return array
+	 */
+	public static function terms_info( $field, $term_ids, $args ) {
+		$args = wp_parse_args( array(
+			'include'    => $term_ids,
+			'hide_empty' => false,
+		), $args );
+
+		$info = get_terms( $field['taxonomy'], $args );
+		$info = is_array( $info ) ? $info : array();
+		return $field['multiple'] ? $info : reset( $info );
 	}
 }
