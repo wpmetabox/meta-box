dc968524327393b82c91eb062ceeb0fda4d28f22
Anh <anhtnt@elightup.com>
2018-05-30 09:58:51 +0700

    Fixed missing styles for autocomplete in the frontend.

--- inc/fields/map.php
+++ inc/fields/map.php
@@ -13,6 +13,8 @@ class RWMB_Map_Field extends RWMB_Field {
 	 * Enqueue scripts and styles.
 	 */
 	public static function admin_enqueue_scripts() {
+		wp_enqueue_style( 'rwmb-map', RWMB_CSS_URL . 'map.css', array( 'forms' ), RWMB_VER );
+
 		/**
 		 * Since June 2016, Google Maps requires a valid API key.
 		 *
@@ -36,7 +38,6 @@ class RWMB_Map_Field extends RWMB_Field {
 		 */
 		$google_maps_url = apply_filters( 'rwmb_google_maps_url', $google_maps_url );
 		wp_register_script( 'google-maps', esc_url_raw( $google_maps_url ), array(), '', true );
-		wp_enqueue_style( 'rwmb-map', RWMB_CSS_URL . 'map.css', array(), RWMB_VER );
 		wp_enqueue_script( 'rwmb-map', RWMB_JS_URL . 'map.js', array(
 			'jquery-ui-autocomplete',
 			'google-maps',
