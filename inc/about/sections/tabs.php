ec673e25858810eaba24e18c882e6e2578b086d4
Anh Tran <anhtnt@elightup.com>
2018-06-07 17:16:23 -0700

    Fixed coding style

--- inc/about/sections/tabs.php
+++ inc/about/sections/tabs.php
@@ -8,7 +8,7 @@
 ?>
 <h2 class="nav-tab-wrapper">
 	<a href="#getting-started" class="nav-tab nav-tab-active"><?php esc_html_e( 'Getting Started', 'meta-box' ); ?></a>
-    <?php do_action( 'rwmb_about_tabs' ); ?>
+	<?php do_action( 'rwmb_about_tabs' ); ?>
 	<a href="#extensions" class="nav-tab"><?php esc_html_e( 'Extensions', 'meta-box' ); ?></a>
 	<a href="#support" class="nav-tab"><?php esc_html_e( 'Support', 'meta-box' ); ?></a>
 </h2>
