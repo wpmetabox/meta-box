dad4d0b6b60a83329913307465ad462ecae2442a
Anh Tran <anhtnt@elightup.com>
2018-12-06 15:49:31 +0700

    Fixed wysiwyg field not saving in Gutenberg.
    Bug report: https://metabox.io/support/topic/data-are-not-saved-into-the-database/
    Issue: https://github.com/WordPress/gutenberg/issues/7176
    
    The fix is based on CMB2 fix (https://github.com/CMB2/CMB2/pull/1190) and the PR(https://github.com/WordPress/gutenberg/pull/12568/)
    
    When the PR is merged into core/Gutenberg, this fix should be removed.

--- js/wysiwyg.js
+++ js/wysiwyg.js
@@ -89,6 +89,23 @@ jQuery( function ( $ ) {
 		        .find( '.quicktags-toolbar' ).attr( 'id', 'qt_' + id + '_toolbar' ).html( '' );
 	}
 
+	/**
+	 * Handles updating tiny mce instances when saving a gutenberg post.
+	 * https://metabox.io/support/topic/data-are-not-saved-into-the-database/
+	 * https://github.com/WordPress/gutenberg/issues/7176
+	 */
+	function ensureSave() {
+		if ( ! wp.data || ! window.tinyMCE ) {
+			return;
+		}
+		wp.data.subscribe( function() {
+			if ( wp.data.select( 'core/editor' ).isSavingPost() ) {
+				window.tinyMCE.triggerSave();
+			}
+		} );
+	}
+
 	$( '.rwmb-wysiwyg' ).each( update );
 	$( document ).on( 'clone', '.rwmb-wysiwyg', update );
+	ensureSave();
 } );
