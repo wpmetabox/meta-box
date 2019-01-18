fffb6e0bb6502517da0b3b109b98e1c1e2d671f0
anhtu91 <tudt@elightup.com>
2019-01-18 16:10:31 +0700

    Check select2 khi clone , group

--- js/select-tree.js
+++ js/select-tree.js
@@ -5,7 +5,6 @@ jQuery( function ( $ ) {
 		var $this = $( this ),
 			required = $this.prop( 'required' );
 
-		$this.select2();
 		if ( required ) {
 			$this.data( 'initial-required', required );
 		}
@@ -27,12 +26,19 @@ jQuery( function ( $ ) {
 		var $this = $( this ),
 			val = $this.val(),
 			$selected = $this.siblings( "[data-parent-id='" + val + "']" ),
-			$notSelected = $this.siblings().not( $selected );
+			$notSelected = $this.siblings().not( $selected ),
+			options = $this.data( 'options' );
+
+		// processing option select2
+		$this.removeClass( 'select2-hidden-accessible' );
+		$this.siblings( '.select2-container' ).remove();
+		$this.show().select2( options );
 
 		$selected.removeClass( 'hidden' ).find( 'select' ).each( setRequiredProp );
 		$notSelected.addClass( 'hidden' ).find( 'select' ).each( unsetRequiredProp ).prop( 'selectedIndex', 0 );
 	}
 
+	$( '.rwmb-select-tree select' ).select2();
 	$( '.rwmb-select-tree select' ).each( setInitialRequiredProp );
 	$( '.rwmb-input' )
 		.on( 'change', '.rwmb-select-tree select', update )
