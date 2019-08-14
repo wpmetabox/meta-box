03a131aaf86867cce15013f0b869897c19ee85e7
Anh Tran <anhtnt@elightup.com>
2019-08-14 17:34:53 +0700

    Improve sanitization
    Order of sanitizers (from top to bottom):
    - Via sanitization_callback
    - Via WP sanitization callback, defined in the class
    - Via class method sanitization callback
    
    Some sanitization is opinioned and need to be checked later.

--- inc/sanitizer.php
+++ inc/sanitizer.php
@@ -15,44 +15,53 @@ class RWMB_Sanitizer {
 	 * @var array
 	 */
 	private $callbacks = array(
+		// Built-in WordPress callbacks.
+		'date'       => 'sanitize_text_field',
+		'datetime'   => 'sanitize_text_field',
 		'email'      => 'sanitize_email',
 		'file_input' => 'esc_url_raw',
 		'oembed'     => 'esc_url_raw',
+		'post'       => 'absint',
+		'text'       => 'sanitize_text_field',
+		'textarea'   => 'wp_kses_post',
+		'time'       => 'sanitize_text_field',
 		'url'        => 'esc_url_raw',
+		'user'       => 'absint',
+		'wysiwyg'    => 'wp_kses_post',
 	);
 
 	/**
 	 * Register hook to sanitize field value.
 	 */
 	public function init() {
-		add_filter( 'rwmb_value', array( $this, 'run_sanitize_callback' ), 10, 4 );
-
-		// Built-in callback.
-		foreach ( $this->callbacks as $type => $callback ) {
-			add_filter( "rwmb_{$type}_value", $callback );
-		}
-
-		// Custom callback.
-		$methods = array_diff( get_class_methods( __CLASS__ ), array( 'init', 'run_sanitize_callback' ) );
-		foreach ( $methods as $method ) {
-			$type = substr( $method, 9 );
-			add_filter( "rwmb_{$type}_value", array( $this, $method ) );
-		}
+		add_filter( 'rwmb_value', array( $this, 'sanitize' ), 10, 4 );
 	}
 
 	/**
-	 * Run `sanitize_callback` for each field if it's defined.
+	 * Sanitize a field value.
 	 *
 	 * @param mixed $value     The submitted new value.
 	 * @param array $field     The field settings.
 	 * @param mixed $old_value The old field value in the database.
 	 * @param int   $object_id The object ID.
 	 */
-	public function run_sanitize_callback( $value, $field, $old_value, $object_id ) {
-		if ( ! is_callable( $field['sanitize_callback'] ) ) {
-			return $value;
+	public function sanitize( $value, $field, $old_value, $object_id ) {
+		$type = $field['type'];
+
+		// User-defined callback.
+		$callback = $field['sanitize_callback'];
+
+		// Built-in WordPress callback.
+		if ( ! is_callable( $callback ) && isset( $this->callbacks[ $type ] ) ) {
+			$callback = $this->callbacks[ $type ];
 		}
-		return call_user_func( $field['sanitize_callback'], $value, $old_value, $field, $object_id );
+
+		// Custom callback.
+		if ( ! is_callable( $callback ) && method_exists( $this, "sanitize_$type" ) ) {
+			$callback = array( $this, "sanitize_$type" );
+		}
+
+		return is_callable( $callback ) ? call_user_func( $callback, $value, $old_value, $field, $object_id ) : $value;
 	}
 
 	/**
@@ -63,7 +72,7 @@ class RWMB_Sanitizer {
 	 * @param string $value Checkbox value.
 	 * @return int
 	 */
-	public function sanitize_checkbox( $value ) {
+	private function sanitize_checkbox( $value ) {
 		return (int) ! empty( $value );
 	}
 
@@ -74,7 +83,38 @@ class RWMB_Sanitizer {
 	 * @param string $value Switch value.
 	 * @return int
 	 */
-	public function sanitize_switch( $value ) {
+	private function sanitize_switch( $value ) {
 		return (int) ! empty( $value );
+	}
+
+	/**
+	 * Sanitize numeric value.
+	 *
+	 * @param string $value The number value.
+	 * @return int|float
+	 */
+	private function sanitize_number( $value ) {
+		return is_numeric( $value ) ? $value : 0;
+	}
+
+	/**
+	 * Sanitize color value.
+	 *
+	 * @param string $value The color value.
+	 * @return string
+	 */
+	private function sanitize_color( $value ) {
+		if ( false === strpos( $value, 'rgba' ) ) {
+			return sanitize_hex_color( $value );
+		}
+
+		// rgba value.
+		$red   = '';
+		$green = '';
+		$blue  = '';
+		$alpha = '';
+		sscanf( $value, 'rgba(%d,%d,%d,%f)', $red, $green, $blue, $alpha );
+
+		return 'rgba(' . $red . ',' . $green . ',' . $blue . ',' . $alpha . ')';
 	}
 }
