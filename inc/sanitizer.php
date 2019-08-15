b89849c514155a7e474de4b0b2fb31814d37a4e0
Anh Tran <anhtnt@elightup.com>
2019-08-15 09:31:01 +0700

    Simplify get callback logic

--- inc/sanitizer.php
+++ inc/sanitizer.php
@@ -10,27 +10,6 @@
  */
 class RWMB_Sanitizer {
 	/**
-	 * Built-in callbacks for some specific types.
-	 *
-	 * @var array
-	 */
-	private $callbacks = array(
-		// Built-in WordPress callbacks.
-		'date'       => 'sanitize_text_field',
-		'datetime'   => 'sanitize_text_field',
-		'email'      => 'sanitize_email',
-		'file_input' => 'esc_url_raw',
-		'oembed'     => 'esc_url_raw',
-		'post'       => 'absint',
-		'text'       => 'sanitize_text_field',
-		'textarea'   => 'wp_kses_post',
-		'time'       => 'sanitize_text_field',
-		'url'        => 'esc_url_raw',
-		'user'       => 'absint',
-		'wysiwyg'    => 'wp_kses_post',
-	);
-
-	/**
 	 * Register hook to sanitize field value.
 	 */
 	public function init() {
@@ -46,22 +25,45 @@ class RWMB_Sanitizer {
 	 * @param int   $object_id The object ID.
 	 */
 	public function sanitize( $value, $field, $old_value, $object_id ) {
-		$type = $field['type'];
-
-		// User-defined callback.
-		$callback = $field['sanitize_callback'];
-
-		// Built-in WordPress callback.
-		if ( ! is_callable( $callback ) && isset( $this->callbacks[ $type ] ) ) {
-			$callback = $this->callbacks[ $type ];
-		}
-
-		// Custom callback.
-		if ( ! is_callable( $callback ) && method_exists( $this, "sanitize_$type" ) ) {
-			$callback = array( $this, "sanitize_$type" );
-		}
+		$callback = $this->get_callback( $field );
 
 		return is_callable( $callback ) ? call_user_func( $callback, $value, $old_value, $field, $object_id ) : $value;
+	}
+
+	/**
+	 * Get sanitize callback for a field.
+	 *
+	 * @param  array $field Field settings.
+	 * @return callable
+	 */
+	private function get_callback( $field ) {
+		// User-defined callback.
+		if ( is_callable( $field['sanitize_callback'] ) ) {
+			return $field['sanitize_callback'];
+		}
+
+		$callbacks = array(
+			'checkbox'   => array( $this, 'sanitize_checkbox' ),
+			'color'      => array( $this, 'sanitize_color' ),
+			'date'       => 'sanitize_text_field',
+			'datetime'   => 'sanitize_text_field',
+			'email'      => 'sanitize_email',
+			'file_input' => 'esc_url_raw',
+			'number'     => array( $this, 'sanitize_number' ),
+			'oembed'     => 'esc_url_raw',
+			'post'       => 'absint',
+			'switch'     => array( $this, 'sanitize_checkbox' ),
+			'text'       => 'sanitize_text_field',
+			'textarea'   => 'wp_kses_post',
+			'time'       => 'sanitize_text_field',
+			'url'        => 'esc_url_raw',
+			'user'       => 'absint',
+			'wysiwyg'    => 'wp_kses_post',
+		);
+
+		$type = $field['type'];
+
+		return isset( $callbacks[ $type ] ) ? $callbacks[ $type ] : null;
 	}
 
 	/**
@@ -73,17 +75,6 @@ class RWMB_Sanitizer {
 	 * @return int
 	 */
 	private function sanitize_checkbox( $value ) {
-		return (int) ! empty( $value );
-	}
-
-	/**
-	 * Set the value of switch to 1 or 0 instead of 'checked' and empty string.
-	 * This prevents using default value once the switch has been unchecked.
-	 *
-	 * @param string $value Switch value.
-	 * @return int
-	 */
-	private function sanitize_switch( $value ) {
 		return (int) ! empty( $value );
 	}
 
