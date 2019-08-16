bd8acf6d99ddefcc570e0290b881462fa1f4e731
Anh Tran <anhtnt@elightup.com>
2019-08-16 10:50:32 +0700

    Optimize save method for taxonomy advanced field

--- inc/fields/taxonomy-advanced.php
+++ inc/fields/taxonomy-advanced.php
@@ -34,26 +34,8 @@ class RWMB_Taxonomy_Advanced_Field extends RWMB_Taxonomy_Field {
 	 * @param array $field   The field parameters.
 	 */
 	public static function save( $new, $old, $post_id, $field ) {
-		if ( empty( $field['id'] ) || ! $field['save_field'] ) {
-			return;
-		}
-		$storage = $field['storage'];
-
-		if ( ! $new ) {
-			$storage->delete( $post_id, $field['id'] );
-			return;
-		}
-
-		if ( ! $field['clone'] || ! $field['clone_as_multiple'] ) {
-			$storage->update( $post_id, $field['id'], $new );
-			return;
-		}
-
-		// clone and clone_as_multiple.
-		$storage->delete( $post_id, $field['id'] );
-		foreach ( $new as $value ) {
-			$storage->add( $post_id, $field['id'], $value );
-		}
+		$field['multiple'] = false; // Force to save in 1 row in the database.
+		RWMB_Field::save( $new, $old, $post_id, $field );
 	}
 
 	/**
