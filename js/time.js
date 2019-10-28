aa3f5c6d9f24af86000777471a96777985195863
Anh Tran <anhtnt@elightup.com>
2019-10-28 08:57:37 +0700

    Don't show date picker for readonly fields

--- js/time.js
+++ js/time.js
@@ -15,6 +15,11 @@
 		options.onSelect = function() {
 			$this.trigger( 'change' );
 		}
+		options.beforeShow = function( i ) {
+			if ( $( i ).prop( 'readonly' ) ) {
+				return false;
+			}
+		}
 
 		if ( ! $inline.length ) {
 			$this.removeClass( 'hasDatepicker' ).timepicker( options );
