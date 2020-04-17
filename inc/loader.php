5e5c9415358815b6baacd724d22e08961319ef63
Anh Tran <anhtnt@elightup.com>
2020-04-17 11:22:46 +0700

    Version 5.2.9

--- inc/loader.php
+++ inc/loader.php
@@ -18,7 +18,7 @@ class RWMB_Loader {
 	 */
 	protected function constants() {
 		// Script version, used to add version for scripts and styles.
-		define( 'RWMB_VER', '5.2.8' );
+		define( 'RWMB_VER', '5.2.9' );
 
 		list( $path, $url ) = self::get_path( dirname( dirname( __FILE__ ) ) );
 
