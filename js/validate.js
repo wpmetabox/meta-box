71c9e72baa1e5031307b47a5ab72f5320098d71c
Anh Tran <anhtnt@elightup.com>
2020-05-07 10:21:23 +0700

    Call valid() to show errors!
    Also optimize invalidHandler

--- js/validate.js
+++ js/validate.js
@@ -10,7 +10,6 @@
 		}
 
 		init() {
-
 			this.$form.on( 'submit', function() {
 				// Update underlying textarea before submit.
 				if ( typeof tinyMCE !== 'undefined' ) {
@@ -37,73 +36,66 @@
 		}
 
 		getSettings() {
-			var that = this;
-			this.settings = this.getGeneralSettings();
-			this.settings.invalidHandler = function () {
-				// Re-enable the submit ( publish/update ) button and hide the ajax indicator
-				$( '#publish' ).removeClass( 'button-primary-disabled' );
-				$( '#ajax-loading' ).attr( 'style', '' );
-				$( '#rwmb-validation-message' ).remove();
-				that.$form.before( '<div id="rwmb-validation-message" class="notice notice-error is-dismissible"><p>' + i18n.message + '</p></div>' );
-
-				// Custom event for showing error fields inside tabs/hidden divs. Use setTimeout() to run after error class is added to inputs.
-				setTimeout( function() {
-					that.$form.trigger( 'after_validate' );
-				}, 200 );
-			};
-		}
-
-		getGeneralSettings() {
-			var settings = {
+			this.settings = {
 				ignore: ':not([class|="rwmb"]:visible)',
 				errorPlacement: function( error, element ) {
 					error.appendTo( element.closest( '.rwmb-input' ) );
 				},
 				errorClass: 'rwmb-error',
 				errorElement: 'p',
+				invalidHandler: this.error.bind( this )
 			};
 
 			// Gather all validation rules.
+			var that = this;
 			this.validationElements.each( function () {
-				$.extend( true, settings, $( this ).data( 'validation' ) );
+				$.extend( true, that.settings, $( this ).data( 'validation' ) );
 			} );
+		}
 
-			return settings;
+		error() {
+			var that = this;
+
+			// Re-enable the submit ( publish/update ) button and hide the ajax indicator
+			$( '#publish' ).removeClass( 'button-primary-disabled' );
+			$( '#ajax-loading' ).attr( 'style', '' );
+			$( '#rwmb-validation-message' ).remove();
+			that.$form.before( '<div id="rwmb-validation-message" class="notice notice-error is-dismissible"><p>' + i18n.message + '</p></div>' );
+
+			// Custom event for showing error fields inside tabs/hidden divs. Use setTimeout() to run after error class is added to inputs.
+			setTimeout( function() {
+				that.$form.trigger( 'after_validate' );
+			}, 200 );
 		}
 	};
 
 	class GutenbergValidation extends Validation {
 		init() {
-			var that = this;
-			wp.data.dispatch( 'core/editor' ).savePost = function() {
+			var that = this,
+				editor = wp.data.dispatch( 'core/editor' ),
+				savePost = editor.savePost; // Reference original method.
+
+			editor.savePost = function() {
 				that.$form.validate( that.settings );
-			}
+
+				if ( that.$form.valid() ) {
+					// Call the original method.
+					savePost();
+				}
+			};
 		}
 
-		getSettings() {
+		error() {
 			var that = this;
 
-			this.settings = this.getGeneralSettings();
+			wp.data.dispatch( 'core/notices' ).createErrorNotice( i18n.message, {
+				id: 'meta-box-validation',
+				isDismissible: true
+			} );
 
-			// Reference original method.
-			this.savePost = wp.data.dispatch( 'core/editor' ).savePost;
-
-			this.settings.invalidHandler = function() {
-				wp.data.dispatch( 'core/notices' ).createErrorNotice( i18n.message, {
-					id: 'meta-box-validation',
-					isDismissible: true
-				} );
-				wp.data.dispatch( 'core/editor' ).lockPostSaving( 'meta_box' );
-
-				setTimeout( function() {
-					that.$form.trigger( 'after_validate' );
-				}, 200 );
-			};
-
-			this.settings.submitHandler = function() {
-				// Call original savePost method.
-				that.savePost();
-			};
+			setTimeout( function() {
+				that.$form.trigger( 'after_validate' );
+			}, 200 );
 		}
 	};
 
