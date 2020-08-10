bc93d000f71f3c2266b11601898dc96510790d5d
Simon <40397142+aboutsimon@users.noreply.github.com>
2020-08-10 19:18:27 +0200

    fix autoloader.php

--- inc/autoloader.php
+++ inc/autoloader.php
@@ -56,7 +56,7 @@ class RWMB_Autoloader {
 				$file = substr( $file, 0, - strlen( $dir['suffix'] ) );
 			}
 			if ( function_exists( 'mb_strtolower' ) && function_exists( 'mb_detect_encoding' )  ) {
-				$file = mb_strtolower( str_replace( '_', '-', $file ), mb_detect_encoding( $file ) ) . '.php'
+				$file = mb_strtolower( str_replace( '_', '-', $file ), mb_detect_encoding( $file ) ) . '.php';
 			} else {
 				$file = strtolower( str_replace( '_', '-', $file ) ) . '.php';
 			}
