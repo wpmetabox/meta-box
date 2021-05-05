1aee6ac111f49322cf273da149a5c1bf13d4b1d5
LyNguyen <cnttnt@gmail.com>
2021-05-05 17:31:53 +0700

    Fix not saving timestamp for date field via REST API
    * fix  update timestamp for date field
    
    * check 2 cases save post and update by API
    
    * check if client use timestamp in API
    
    * add comment
    
    * change check case flow
    
    * use tab instead space

--- inc/fields/datetime.php
+++ inc/fields/datetime.php
@@ -141,7 +141,12 @@ class RWMB_Datetime_Field extends RWMB_Text_Field {
 	 */
 	public static function value( $new, $old, $post_id, $field ) {
 		if ( $field['timestamp'] ) {
-			return $new['timestamp'];
+			if ( is_array( $new ) ) {
+				return $new['timestamp'];
+			} elseif ( ! is_numeric( $new ) ) {
+				return strtotime( $new );
+			}
+			return $new;
 		}
 
 		if ( $field['save_format'] ) {
