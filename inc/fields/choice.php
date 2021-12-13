b49acf787ac71b5123d82f5c6bbbc289a9124d33
lexuanbao161096 <lexuanbao161096@gmail.com>
2021-12-13 11:57:39 +0700

    check lại điều kiện

--- inc/fields/choice.php
+++ inc/fields/choice.php
@@ -36,6 +36,12 @@ abstract class RWMB_Choice_Field extends RWMB_Field {
 			)
 		);
 
+		// Use callback: function_name format.
+		if ( isset( $field['_callback']) ) {
+			$callback          = trim( $field['_callback'] );
+			$field['options']  = is_callable( $callback ) ? call_user_func( $callback ) : array();
+		}
+
 		return $field;
 	}
 
@@ -48,13 +54,6 @@ abstract class RWMB_Choice_Field extends RWMB_Field {
 	 */
 	public static function transform_options( $options ) {
 		$transformed = array();
-
-		// Use callback: function_name format.
-		if ( count( $options ) === 1 && array_keys( $options ) === ['callback'] ) {
-			$callback = trim( $options['callback'] );
-			$options  = is_callable( $callback ) ? call_user_func( $callback ) : array();
-		}
-
 		$options     = (array) $options;
 		foreach ( $options as $value => $label ) {
 			$option = is_array( $label ) ? $label : array(
