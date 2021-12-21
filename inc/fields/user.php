5e0934cdfbf6048a8cae54f6ec73ec5da103a847
Anh Tran <anhtnt@elightup.com>
2021-12-21 08:54:21 +0700

    Fix placeholder typo for user field. Close #1420

--- inc/fields/user.php
+++ inc/fields/user.php
@@ -81,7 +81,7 @@ class RWMB_User_Field extends RWMB_Object_Choice_Field {
 		$field = wp_parse_args(
 			$field,
 			array(
-				'placeholder'   => __( 'Select an user', 'meta-box' ),
+				'placeholder'   => __( 'Select a user', 'meta-box' ),
 				'query_args'    => array(),
 				'display_field' => 'display_name',
 			)
