e5ae098d18bb0a99a4abae7ce9ca0e89084f2026
vietsmiler <lqviet.it@gmail.com>
2022-04-13 08:58:50 +0700

    Revert "validation.js"
    This reverts commit 99528bb8b3e05f7292a7daf213d938a8221801c8.

--- js/validation/validation.js
+++ js/validation/validation.js
@@ -1,131 +1,128 @@
 ( function( $, rwmb, i18n ) {
-    'use strict';
+	'use strict';
 
-    class Validation {
-        constructor( formSelector ) {
-            this.$form = $( formSelector );
-            this.validationElements = this.$form.find( '.rwmb-validation' );
-            this.showAsterisks();
-            this.getSettings();
-        }
+	class Validation {
+		constructor( formSelector ) {
+			this.$form = $( formSelector );
+			this.validationElements = this.$form.find( '.rwmb-validation' );
+			this.showAsterisks();
+			this.getSettings();
+		}
 
-        init() {
-            this.$form
-                // Update underlying textarea before submit.
-                // Don't use submitHandler() because form can be submitted via Ajax on the front end.
-                .on( 'submit', function() {
-                    if ( typeof tinyMCE !== 'undefined' ) {
-                        tinyMCE.triggerSave();
-                    }
-                } )
-                .validate( this.settings );
-        }
+		init() {
+			this.$form
+				// Update underlying textarea before submit.
+				// Don't use submitHandler() because form can be submitted via Ajax on the front end.
+				.on( 'submit', function() {
+					if ( typeof tinyMCE !== 'undefined' ) {
+						tinyMCE.triggerSave();
+					}
+				} )
+				.validate( this.settings );
+		}
 
-        showAsterisks() {
-            this.validationElements.each( function() {
-                var data = $( this ).data( 'validation' );
+		showAsterisks() {
+			this.validationElements.each( function() {
+				var data = $( this ).data( 'validation' );
 
-                $.each( data.rules, function( k, v ) {
-                    if ( !v[ 'required' ] ) {
-                        return;
-                    }
-                    var $el = $( '[name="' + k + '"]' );
-                    if ( !$el.length ) {
-                        return;
-                    }
-                    $el.closest( '.rwmb-input' ).siblings( '.rwmb-label' ).find( 'label' ).append( '<span class="rwmb-required">*</span>' );
-                } );
-            } );
-        }
+				$.each( data.rules, function( k, v ) {
+					if ( !v[ 'required' ] ) {
+						return;
+					}
+					var $el = $( '[name="' + k + '"]' );
+					if ( !$el.length ) {
+						return;
+					}
+					$el.closest( '.rwmb-input' ).siblings( '.rwmb-label' ).find( 'label' ).append( '<span class="rwmb-required">*</span>' );
+				} );
+			} );
+		}
 
-        getSettings() {
-            this.settings = {
-                ignore: ':not(.rwmb-media,.rwmb-image_select,.rwmb-wysiwyg,.rwmb-color,.rwmb-map,.rwmb-osm,.rwmb-switch,[class|="rwmb"]:visible,[class|="rwmb"]:hidden)',
-                errorPlacement: function( error, element ) {
-                    error.appendTo( element.closest( '.rwmb-input' ) );
-                },
-                errorClass: 'rwmb-error',
-                errorElement: 'p',
-                invalidHandler: this.invalidHandler.bind( this )
-            };
+		getSettings() {
+			this.settings = {
+				ignore: ':not(.rwmb-media,.rwmb-image_select,.rwmb-wysiwyg,.rwmb-color,.rwmb-map,.rwmb-osm,.rwmb-switch,[class|="rwmb"]:visible)',
+				errorPlacement: function( error, element ) {
+					error.appendTo( element.closest( '.rwmb-input' ) );
+				},
+				errorClass: 'rwmb-error',
+				errorElement: 'p',
+				invalidHandler: this.invalidHandler.bind( this )
+			};
 
-            // Gather all validation rules.
-            var that = this;
-            this.validationElements.each( function() {
-                $.extend( true, that.settings, $( this ).data( 'validation' ) );
-            } );
-        }
+			// Gather all validation rules.
+			var that = this;
+			this.validationElements.each( function() {
+				$.extend( true, that.settings, $( this ).data( 'validation' ) );
+			} );
+		}
 
-        invalidHandler() {
-            this.showMessage();
-            // Group field will automatically expand and show an error warning when collapsing
-            for ( var i = 0; i<this.$form.data( 'validator' ).errorList.length; i++ ){
-                $( '#' + this.$form.data( 'validator' ).errorList[i].element.id ).closest( '.rwmb-group-collapsed' ).removeClass( 'rwmb-group-collapsed' );
-            }
-            // Custom event for showing error fields inside tabs/hidden divs. Use setTimeout() to run after error class is added to inputs.
-            var that = this;
-            setTimeout( function() {
-                that.$form.trigger( 'after_validate' );
-            }, 200 );
-        }
+		invalidHandler() {
+			this.showMessage();
 
-        showMessage() {
-            // Re-enable the submit ( publish/update ) button and hide the ajax indicator
-            $( '#publish' ).removeClass( 'button-primary-disabled' );
-            $( '#ajax-loading' ).attr( 'style', '' );
-            $( '#rwmb-validation-message' ).remove();
-            this.$form.before( '<div id="rwmb-validation-message" class="notice notice-error is-dismissible"><p>' + i18n.message + '</p></div>' );
-        }
-    };
+			// Custom event for showing error fields inside tabs/hidden divs. Use setTimeout() to run after error class is added to inputs.
+			var that = this;
+			setTimeout( function() {
+				that.$form.trigger( 'after_validate' );
+			}, 200 );
+		}
 
-    class GutenbergValidation extends Validation {
-        init() {
-            var that = this,
-                editor = wp.data.dispatch( 'core/editor' ),
-                savePost = editor.savePost; // Reference original method.
+		showMessage() {
+			// Re-enable the submit ( publish/update ) button and hide the ajax indicator
+			$( '#publish' ).removeClass( 'button-primary-disabled' );
+			$( '#ajax-loading' ).attr( 'style', '' );
+			$( '#rwmb-validation-message' ).remove();
+			this.$form.before( '<div id="rwmb-validation-message" class="notice notice-error is-dismissible"><p>' + i18n.message + '</p></div>' );
+		}
+	};
 
-            // Change the editor method.
-            editor.savePost = function( object ) {
-                // Bypass the validation when previewing in Gutenberg.
-                if ( typeof object === 'object' && object.isPreview ) {
-                    savePost( object );
-                    return;
-                }
-                that.$form.validate( that.settings );
+	class GutenbergValidation extends Validation {
+		init() {
+			var that = this,
+				editor = wp.data.dispatch( 'core/editor' ),
+				savePost = editor.savePost; // Reference original method.
 
-                // Must call savePost() here instead of in submitHandler() because the form has inline onsubmit callback.
-                if ( that.$form.valid() ) {
-                    savePost( object );
-                }
-            };
-        }
+			// Change the editor method.
+			editor.savePost = function( object ) {
+				// Bypass the validation when previewing in Gutenberg.
+				if ( typeof object === 'object' && object.isPreview ) {
+					savePost( object );
+					return;
+				}
+				that.$form.validate( that.settings );
 
-        showMessage() {
-            wp.data.dispatch( 'core/notices' ).createErrorNotice( i18n.message, {
-                id: 'meta-box-validation',
-                isDismissible: true
-            } );
-        }
-    };
+				// Must call savePost() here instead of in submitHandler() because the form has inline onsubmit callback.
+				if ( that.$form.valid() ) {
+					savePost( object );
+				}
+			};
+		}
 
-    // Run on document ready.
-    $( function() {
-        if ( rwmb.isGutenberg ) {
-            var advanced = new GutenbergValidation( '.metabox-location-advanced' ),
-                normal = new GutenbergValidation( '.metabox-location-normal' ),
-                side = new GutenbergValidation( '.metabox-location-side' );
+		showMessage() {
+			wp.data.dispatch( 'core/notices' ).createErrorNotice( i18n.message, {
+				id: 'meta-box-validation',
+				isDismissible: true
+			} );
+		}
+	};
 
-            side.init();
-            normal.init();
-            advanced.init();
-            return;
-        }
+	// Run on document ready.
+	$( function() {
+		if ( rwmb.isGutenberg ) {
+			var advanced = new GutenbergValidation( '.metabox-location-advanced' ),
+				normal = new GutenbergValidation( '.metabox-location-normal' ),
+				side = new GutenbergValidation( '.metabox-location-side' );
 
-        // Edit post, edit term, edit user, front-end form.
-        var $forms = $( '#post, #edittag, #your-profile, .rwmb-form' );
-        $forms.each( function() {
-            var form = new Validation( this );
-            form.init();
-        } );
-    } );
+			side.init();
+			normal.init();
+			advanced.init();
+			return;
+		}
+
+		// Edit post, edit term, edit user, front-end form.
+		var $forms = $( '#post, #edittag, #your-profile, .rwmb-form' );
+		$forms.each( function() {
+			var form = new Validation( this );
+			form.init();
+		} );
+	} );
 } )( jQuery, rwmb, rwmbValidation );
