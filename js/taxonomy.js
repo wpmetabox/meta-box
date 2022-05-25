6990019c249837dee0c9c4b916c0fbd22a751786
kiet_DEV <tuankiet2605@gmail.com>
2022-05-25 16:38:50 +0700

    Field Taxonomy: Does not create new term when add new

--- js/taxonomy.js
+++ js/taxonomy.js
@@ -1,10 +1,24 @@
 ( function ( $, rwmb ) {
 	'use strict';
+    var required = true;
 
 	function toggleAddInput( e ) {
 		e.preventDefault();
 		this.nextElementSibling.classList.toggle( 'rwmb-hidden' );
 	}
 
+    function focusOutInput( ) {
+        if( $( this ).val( ) != '' ) {
+            required = false;
+        } else {
+            required = true;
+        }
+
+        $( this ).parents( ".rwmb-input" ).find( rwmb.inputSelectors ).rules( 'add', {
+            required: required
+        } );
+    }
+
+    rwmb.$document.on( 'blur', '.rwmb-taxonomy-add-form input', focusOutInput );
 	rwmb.$document.on( 'click', '.rwmb-taxonomy-add-button', toggleAddInput );
 } )( jQuery, rwmb );
