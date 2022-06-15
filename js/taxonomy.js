a619928350f20201bf0ef144c7bffb99192bae9d
kiet_DEV <tuankiet2605@gmail.com>
2022-06-15 08:55:49 +0700

    Fix reload after add new taxo

--- js/taxonomy.js
+++ js/taxonomy.js
@@ -14,6 +14,21 @@
         $( this ).closest( '.rwmb-input' ).find( rwmb.inputSelectors ).removeClass( 'rwmb-error' );
     }
 
+    var saved = true;
+    wp.data.subscribe(function () {
+        if (wp.data.select('core/editor').isSavingPost()) {
+            saved = false;
+        } else {
+            if (!saved) {
+                saved = true;
+                setTimeout(() => {
+                    window.location.reload(true);
+                }, 2000);
+            }
+
+        }
+    });
+
     rwmb.$document.on( 'blur', '.rwmb-taxonomy-add-form input', focusOutInput );
 	rwmb.$document.on( 'click', '.rwmb-taxonomy-add-button', toggleAddInput );
 } )( jQuery, rwmb );
