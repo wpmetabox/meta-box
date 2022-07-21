fc09f12d31e9b9f10c7ac79a1de7d17c7a3c5a70
Anh Tran <anhtnt@elightup.com>
2022-07-21 14:28:33 +0700

    Format code

--- js/taxonomy.js
+++ js/taxonomy.js
@@ -7,11 +7,10 @@
 	}
 
     function focusOutInput() {
-        const required = $( this ).val( ) == '';
-        $( this ).closest( '.rwmb-input' ).find( rwmb.inputSelectors ).rules( 'add', {
+        const required = $( this ).val() == '';
+        $( this ).closest( '.rwmb-input' ).find( rwmb.inputSelectors ).removeClass( 'rwmb-error' ).rules( 'add', {
             required
         } );
-        $( this ).closest( '.rwmb-input' ).find( rwmb.inputSelectors ).removeClass( 'rwmb-error' );
     }
 
     rwmb.$document.on( 'blur', '.rwmb-taxonomy-add-form input', focusOutInput );
