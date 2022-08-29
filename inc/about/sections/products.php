c9fb82cc39e59ef4bc3e381268dd5ea9a5b083e1
Anh Tran <anhtnt@elightup.com>
2022-08-29 08:20:17 +0700

    Fix wrong text domain. Closes #1460

--- inc/about/sections/products.php
+++ inc/about/sections/products.php
@@ -9,11 +9,11 @@
 
 <div class="postbox">
 	<h3 class="hndle">
-		<span><?php esc_html_e( 'Our WordPress Products', 'falcon' ) ?></span>
+		<span><?php esc_html_e( 'Our WordPress Products', 'meta-box' ) ?></span>
 	</h3>
 	<div class="inside">
-		<p><?php esc_html_e( 'Like this plugin? Check out our other WordPress products:', 'falcon' ) ?></p>
-		<p><a href="https://wpslimseo.com?utm_source=WordPress&utm_medium=link&utm_campaign=meta-box" target="_blank" rel="noopenner noreferrer">Slim SEO</a> - <?php esc_html_e( 'Automated & fast SEO plugin for WordPress', 'falcon' ) ?></p>
-		<p><a href="https://gretathemes.com/wordpress-themes/estar/?utm_source=WordPress&utm_medium=link&utm_campaign=meta-box" target="_blank" rel="noopenner noreferrer">eStar</a> - <?php esc_html_e( 'A super fast, lightweight and highly customizable WordPress theme', 'falcon' ) ?></p>
+		<p><?php esc_html_e( 'Like this plugin? Check out our other WordPress products:', 'meta-box' ) ?></p>
+		<p><a href="https://wpslimseo.com?utm_source=WordPress&utm_medium=link&utm_campaign=meta-box" target="_blank" rel="noopenner noreferrer">Slim SEO</a> - <?php esc_html_e( 'Automated & fast SEO plugin for WordPress', 'meta-box' ) ?></p>
+		<p><a href="https://gretathemes.com/wordpress-themes/estar/?utm_source=WordPress&utm_medium=link&utm_campaign=meta-box" target="_blank" rel="noopenner noreferrer">eStar</a> - <?php esc_html_e( 'A super fast, lightweight and highly customizable WordPress theme', 'meta-box' ) ?></p>
 	</div>
 </div>
