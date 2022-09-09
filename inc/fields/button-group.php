1a80e25bdf44cd64030ac5b327632085c8313b19
Quốc Việt Lê <lqviet.it@gmail.com>
2022-09-09 17:04:39 +0700

    Fix button group option to display horizontally not save in the builder (#1461)

--- inc/fields/button-group.php
+++ inc/fields/button-group.php
@@ -14,7 +14,7 @@ class RWMB_Button_Group_Field extends RWMB_Choice_Field {
 	 */
 	public static function admin_enqueue_scripts() {
 		wp_enqueue_style( 'rwmb-button-group', RWMB_CSS_URL . 'button-group.css', '', RWMB_VER );
-		wp_enqueue_script( 'rwmb-button-group', RWMB_JS_URL . 'button-group.js', array(), RWMB_VER, true );
+		wp_enqueue_script( 'rwmb-button-group', RWMB_JS_URL . 'button-group.js', [ 'rwmb' ], RWMB_VER, true );
 	}
 
 	/**
@@ -30,7 +30,7 @@ class RWMB_Button_Group_Field extends RWMB_Choice_Field {
 
 		$output  = sprintf(
 			'<ul class="rwmb-button-input-list %s">',
-			$field['inline'] ? ' rwmb-inline' : ''
+			$field['inline'] ? 'rwmb-inline' : ''
 		);
 		$output .= $walker->walk( $options, -1 );
 		$output .= '</ul>';
@@ -47,18 +47,14 @@ class RWMB_Button_Group_Field extends RWMB_Choice_Field {
 	 */
 	public static function normalize( $field ) {
 		$field = parent::normalize( $field );
-		$field = wp_parse_args(
-			$field,
-			array(
-				'inline' => null,
-			)
-		);
+		$field = wp_parse_args( $field, [
+			'inline' => true,
+		] );
 
 		$field = $field['multiple'] ? RWMB_Multiple_Values_Field::normalize( $field ) : $field;
 		$field = RWMB_Input_Field::normalize( $field );
 
 		$field['flatten'] = true;
-		$field['inline']  = ! $field['multiple'] && ! isset( $field['inline'] ) ? true : $field['inline'];
 
 		return $field;
 	}
