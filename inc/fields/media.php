5babc028a3a24e856f5ecf93640147074df9a1ba
Anh Tran <anhtnt@elightup.com>
2022-12-02 11:59:36 +0700

    Modernize code for media.php

--- inc/fields/media.php
+++ inc/fields/media.php
@@ -1,54 +1,36 @@
 <?php
 /**
  * Media field class which users WordPress media popup to upload and select files.
- *
- * @package Meta Box
- */
-
-/**
- * The media field class.
  */
 class RWMB_Media_Field extends RWMB_File_Field {
-	/**
-	 * Enqueue scripts and styles.
-	 */
 	public static function admin_enqueue_scripts() {
 		parent::admin_enqueue_scripts();
 
 		wp_enqueue_media();
 		if ( ! is_admin() ) {
-			wp_register_script( 'media-grid', includes_url( 'js/media-grid.min.js' ), array( 'media-editor' ), '4.9.7', true );
+			wp_register_script( 'media-grid', includes_url( 'js/media-grid.min.js' ), [ 'media-editor' ], '4.9.7', true );
 		}
-		wp_enqueue_style( 'rwmb-media', RWMB_CSS_URL . 'media.css', array(), RWMB_VER );
-		wp_enqueue_script( 'rwmb-media', RWMB_JS_URL . 'media.js', array( 'jquery-ui-sortable', 'underscore', 'backbone', 'media-grid' ), RWMB_VER, true );
+		wp_enqueue_style( 'rwmb-media', RWMB_CSS_URL . 'media.css', [], RWMB_VER );
+		wp_enqueue_script( 'rwmb-media', RWMB_JS_URL . 'media.js', [ 'jquery-ui-sortable', 'underscore', 'backbone', 'media-grid' ], RWMB_VER, true );
 
-		RWMB_Helpers_Field::localize_script_once(
-			'rwmb-media',
-			'i18nRwmbMedia',
-			array(
-				'add'                => apply_filters( 'rwmb_media_add_string', _x( '+ Add Media', 'media', 'meta-box' ) ),
-				'single'             => apply_filters( 'rwmb_media_single_files_string', _x( ' file', 'media', 'meta-box' ) ),
-				'multiple'           => apply_filters( 'rwmb_media_multiple_files_string', _x( ' files', 'media', 'meta-box' ) ),
-				'remove'             => apply_filters( 'rwmb_media_remove_string', _x( 'Remove', 'media', 'meta-box' ) ),
-				'edit'               => apply_filters( 'rwmb_media_edit_string', _x( 'Edit', 'media', 'meta-box' ) ),
-				'view'               => apply_filters( 'rwmb_media_view_string', _x( 'View', 'media', 'meta-box' ) ),
-				'noTitle'            => _x( 'No Title', 'media', 'meta-box' ),
-				'loadingUrl'         => admin_url( 'images/spinner.gif' ),
-				'extensions'         => self::get_mime_extensions(),
-				'select'             => apply_filters( 'rwmb_media_select_string', _x( 'Select Files', 'media', 'meta-box' ) ),
-				'or'                 => apply_filters( 'rwmb_media_or_string', _x( 'or', 'media', 'meta-box' ) ),
-				'uploadInstructions' => apply_filters( 'rwmb_media_upload_instructions_string', _x( 'Drop files here to upload', 'media', 'meta-box' ) ),
-			)
-		);
+		RWMB_Helpers_Field::localize_script_once( 'rwmb-media', 'i18nRwmbMedia', [
+			'add'                => apply_filters( 'rwmb_media_add_string', _x( '+ Add Media', 'media', 'meta-box' ) ),
+			'single'             => apply_filters( 'rwmb_media_single_files_string', _x( ' file', 'media', 'meta-box' ) ),
+			'multiple'           => apply_filters( 'rwmb_media_multiple_files_string', _x( ' files', 'media', 'meta-box' ) ),
+			'remove'             => apply_filters( 'rwmb_media_remove_string', _x( 'Remove', 'media', 'meta-box' ) ),
+			'edit'               => apply_filters( 'rwmb_media_edit_string', _x( 'Edit', 'media', 'meta-box' ) ),
+			'view'               => apply_filters( 'rwmb_media_view_string', _x( 'View', 'media', 'meta-box' ) ),
+			'noTitle'            => _x( 'No Title', 'media', 'meta-box' ),
+			'loadingUrl'         => admin_url( 'images/spinner.gif' ),
+			'extensions'         => static::get_mime_extensions(),
+			'select'             => apply_filters( 'rwmb_media_select_string', _x( 'Select Files', 'media', 'meta-box' ) ),
+			'or'                 => apply_filters( 'rwmb_media_or_string', _x( 'or', 'media', 'meta-box' ) ),
+			'uploadInstructions' => apply_filters( 'rwmb_media_upload_instructions_string', _x( 'Drop files here to upload', 'media', 'meta-box' ) ),
+		] );
 	}
 
-	/**
-	 * Add actions.
-	 */
 	public static function add_actions() {
-		$args  = func_get_args();
-		$field = reset( $args );
-		add_action( 'print_media_templates', array( RWMB_Helpers_Field::get_class( $field ), 'print_templates' ) );
+		add_action( 'print_media_templates', [ get_called_class(), 'print_templates' ] );
 	}
 
 	/**
@@ -89,7 +71,7 @@ class RWMB_Media_Field extends RWMB_File_Field {
 	 * @return string
 	 */
 	public static function html( $meta, $field ) {
-		$attributes = self::call( 'get_attributes', $field, $meta );
+		$attributes = static::get_attributes( $field, $meta );
 
 		$html = sprintf(
 			'<input %s data-options="%s">',
@@ -109,29 +91,23 @@ class RWMB_Media_Field extends RWMB_File_Field {
 	 */
 	public static function normalize( $field ) {
 		$field = parent::normalize( $field );
-		$field = wp_parse_args(
-			$field,
-			array(
-				'std'              => array(),
-				'mime_type'        => '',
-				'max_file_uploads' => 0,
-				'force_delete'     => false,
-				'max_status'       => true,
-				'js_options'       => array(),
-				'add_to'           => 'end',
-			)
-		);
+		$field = wp_parse_args( $field, [
+			'std'              => [],
+			'mime_type'        => '',
+			'max_file_uploads' => 0,
+			'force_delete'     => false,
+			'max_status'       => true,
+			'js_options'       => [],
+			'add_to'           => 'end',
+		] );
 
-		$field['js_options'] = wp_parse_args(
-			$field['js_options'],
-			array(
-				'mimeType'    => $field['mime_type'],
-				'maxFiles'    => $field['max_file_uploads'],
-				'forceDelete' => $field['force_delete'] ? true : false,
-				'maxStatus'   => $field['max_status'],
-				'addTo'       => $field['add_to'],
-			)
-		);
+		$field['js_options'] = wp_parse_args( $field['js_options'], [
+			'mimeType'    => $field['mime_type'],
+			'maxFiles'    => $field['max_file_uploads'],
+			'forceDelete' => $field['force_delete'],
+			'maxStatus'   => $field['max_status'],
+			'addTo'       => $field['add_to'],
+		] );
 
 		$field['multiple'] = true;
 
@@ -157,7 +133,7 @@ class RWMB_Media_Field extends RWMB_File_Field {
 		$attributes['class'] .= ' rwmb-media';
 
 		// Add attachment details.
-		$attachments = array();
+		$attachments = [];
 		foreach ( $value as $media ) {
 			$media = wp_prepare_attachment_for_js( $media );
 			// Some themes/plugins add HTML, shortcodes to "compat" attribute which break JSON validity.
@@ -174,21 +150,16 @@ class RWMB_Media_Field extends RWMB_File_Field {
 		return $attributes;
 	}
 
-	/**
-	 * Get supported mime extensions.
-	 *
-	 * @return array
-	 */
-	protected static function get_mime_extensions() {
+	protected static function get_mime_extensions() : array {
 		$mime_types = wp_get_mime_types();
-		$extensions = array();
+		$extensions = [];
 		foreach ( $mime_types as $ext => $mime ) {
 			$ext                 = explode( '|', $ext );
 			$extensions[ $mime ] = $ext;
 
 			$mime_parts = explode( '/', $mime );
 			if ( empty( $extensions[ $mime_parts[0] ] ) ) {
-				$extensions[ $mime_parts[0] ] = array();
+				$extensions[ $mime_parts[0] ] = [];
 			}
 			$extensions[ $mime_parts[0] ]        = array_merge( $extensions[ $mime_parts[0] ], $ext );
 			$extensions[ $mime_parts[0] . '/*' ] = $extensions[ $mime_parts[0] ];
@@ -233,7 +204,7 @@ class RWMB_Media_Field extends RWMB_File_Field {
 		}
 		$storage = $field['storage'];
 		$storage->delete( $post_id, $field['id'] );
-		parent::save( $new, array(), $post_id, $field );
+		parent::save( $new, [], $post_id, $field );
 	}
 
 	/**
