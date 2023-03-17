50a426471621351c0808b9b85ee7af6dc2cbad97
tuankiet2605 <tuankiet2605@gmail.com>
2023-03-17 11:42:08 +0700

    Remove comment JS

--- js/password.js
+++ js/password.js
@@ -4,8 +4,9 @@
     function toggle() {
         const $this = $( this ).parent().siblings( 'input' );
         $this.attr( 'type', $this.attr( 'type' ) === 'password' ? 'text' : 'password' );
-        $this.attr( 'type' ) === 'password' ? $( this ).removeClass( 'hide-icon' ).addClass( 'show-icon' ) : $( this ).removeClass( 'show-icon' ).addClass( 'hide-icon' );
-        // $( this ).attr( 'class', $this.attr( 'type' ) === 'password' ? 'password-icon show-icon' : 'password-icon hide-icon' );
+        $this.attr( 'type' ) === 'password' ?
+            $( this ).removeClass( 'hide-icon' ).addClass( 'show-icon' ) :
+            $( this ).removeClass( 'show-icon' ).addClass( 'hide-icon' );        
     }
 
     rwmb.$document
