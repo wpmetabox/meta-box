e97b82c8d46729fc19c9f477baaecba88ee1146f
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2023-03-28 15:21:34 +0700

    Add section cho Tutorials vào admin dashboard

--- inc/about/sections/getting-started-pro.php
+++ inc/about/sections/getting-started-pro.php
@@ -3,4 +3,14 @@
 	<div class="youtube-video-container">
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/M0nEF7b0woU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
+	<h3 class="tutorial"><a href="https://docs.metabox.io/tutorials/" target="_blank"><?php esc_html_e( 'Tutorials', 'meta-box' ) ?></a></h3>
+	<p class="about-description"><?php esc_html_e( 'Tutorials provide guidance to create custom meta boxes and fields that meet specific needs:', 'meta-box' ); ?></p>
+	<ul>
+		<li><a href="https://docs.metabox.io/tutorials/beginners/" target="_blank"><?php esc_html_e( 'Beginners', 'meta-box' ); ?></a>: <?php esc_html_e( 'Let’s start with some basic practices with Meta Box.', 'meta-box' ) ?></li>
+		<li><a href="https://docs.metabox.io/tutorials/case-studies/" target="_blank"><?php esc_html_e( 'Case Studies', 'meta-box' ); ?></a>: <?php esc_html_e( 'See how to use Meta Box in the real case studies with comprehensive tutorials.', 'meta-box' ) ?></li>
+		<li><a href="https://docs.metabox.io/tutorials/general-guide/" target="_blank"><?php esc_html_e( 'General Guide', 'meta-box' ); ?></a>: <?php esc_html_e( 'See how to use Meta box in common tasks.', 'meta-box' ) ?></li>
+		<li><a href="https://docs.metabox.io/tutorials/extensions/" target="_blank"><?php esc_html_e( 'Extensions', 'meta-box' ); ?></a>: <?php esc_html_e( 'Learn about Meta Box extensions, what features they offer and how to use them.', 'meta-box' ) ?></li>
+		<li><a href="https://docs.metabox.io/tutorials/builders/" target="_blank"><?php esc_html_e( 'Page Builders', 'meta-box' ); ?></a>: <?php esc_html_e( 'Tutorials on combining Meta Box and other builders or tools for real builders studies.', 'meta-box' ) ?></li>
+		<li><a href="https://docs.metabox.io/tutorials/mb-views/" target="_blank"><?php esc_html_e( 'MB Views', 'meta-box' ); ?></a>: <?php esc_html_e( 'Build front-end templates for WordPress without touching theme files. Support Twig and all field types.', 'meta-box' ) ?></li>
+	</ul>
 </div>
