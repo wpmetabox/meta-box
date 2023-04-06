b2665a9079279135eae12e01aaf9e6f30f1e49c1
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2023-04-06 16:15:46 +0700

    Block register

---
+++ inc/block-register.php
@@ -0,0 +1,15 @@
+<?php
+class RWMB_Block_Register {
+	public function __construct() {
+		add_filter( 'block_categories_all', [ $this, 'register_block_category' ] );
+	}
+
+	public function register_block_category( $categories ) {
+		$categories[] = [
+			'slug'  => 'meta-box',
+			'title' => esc_html__( 'Meta Box', 'meta-box' ),
+		];
+
+		return $categories;
+	}
+}
