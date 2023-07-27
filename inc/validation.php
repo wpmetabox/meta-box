fbae892c0cb32e53381bfb4f2fb0a7e2a800f4ac
Maarten <14947039+ictbeheer@users.noreply.github.com>
2023-07-28 01:51:05 +0200

    Jquery validation i18n (#1525)
    * Localized error messages for jQuery Validation
    
    * Enqueue validation messages

--- inc/validation.php
+++ inc/validation.php
@@ -21,6 +21,14 @@ class RWMB_Validation {
 	public function enqueue() {
 		wp_enqueue_script( 'rwmb-validation', RWMB_JS_URL . 'validation.min.js', [ 'jquery', 'rwmb' ], RWMB_VER, true );
 
+		$locale       = determine_locale();
+		$locale_short = substr( $locale, 0, 2 );
+		$locale       = file_exists( RWMB_DIR . "js/validation/i18n/messages_$locale.js" ) ? $locale : $locale_short;
+
+		if ( file_exists( RWMB_DIR . "js/validation/i18n/messages_$locale.js" ) ) {
+			wp_enqueue_script( 'rwmb-validation-i18n', RWMB_JS_URL . "js/validation/i18n/messages_$locale.js", ['rwmb-validation'], RWMB_VER, true );
+		}
+
 		RWMB_Helpers_Field::localize_script_once( 'rwmb-validation', 'rwmbValidation', [
 			'message' => esc_html( apply_filters( 'rwmb_validation_message_string', __( 'Please correct the errors highlighted below and try again.', 'meta-box' ) ) ),
 		] );
