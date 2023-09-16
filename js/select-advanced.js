862ab08f4c1127ea229afb36fe1d6adb767d07f5
tuankiet2605 <tuankiet2605@gmail.com>
2023-09-16 08:19:52 +0000

    Fix position frontend

--- js/select-advanced.js
+++ js/select-advanced.js
@@ -118,14 +118,12 @@
 			return;
 		}
 
-		if ( $( e.target ).next().hasClass( 'select2-container--above' ) ) {
-			if ( rwmbSelect2.isAdmin == 1 ) {
-				$( 'body > .select2-container--open .select2-dropdown--above' ).css( 'top', 0 );
-				return;
-			}
-
-			$( 'body > .select2-container:last-child > .select2-dropdown' ).css( 'top', $( document.body ).offset().top );
+		if ( rwmbSelect2.isAdmin == 1 ) {
+			$( 'body > .select2-container--open .select2-dropdown--above' ).css( 'top', 0 );
+			return;
 		}
+
+		$( 'body > .select2-container:last-child > .select2-dropdown' ).css( 'top', $( document.body ).offset().top );
 	};
 
 	rwmb.$document
