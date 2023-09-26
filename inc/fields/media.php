417563182420e6ad742515512e975f2f43cd3918
LyNguyen <LyNguyen@example.com>
2023-09-26 10:11:56 +0700

    Fix media template missing in block and customize mode

--- inc/fields/media.php
+++ inc/fields/media.php
@@ -32,9 +32,12 @@ class RWMB_Media_Field extends RWMB_File_Field {
 	}
 
 	public static function add_actions() {
-		// Print HTML templates for Customizer.
-		// Must called here instead of inside html() because content of the field is injected via JavaScript.
-		add_action( 'customize_controls_print_footer_scripts', [ get_called_class(), 'print_templates' ] );
+		if ( is_customize_preview() ) {
+			add_action( 'customize_register', [ get_called_class(), 'print_templates' ] );
+		} else {
+			add_action( 'admin_footer', [ get_called_class(), 'print_templates' ] );
+			add_action( 'wp_footer', [ get_called_class(), 'print_templates' ] );
+		}
 	}
 
 	/**
@@ -75,10 +78,6 @@ class RWMB_Media_Field extends RWMB_File_Field {
 	 * @return string
 	 */
 	public static function html( $meta, $field ) {
-		// Print HTML templates. Runs only when the field is outputted.
-		add_action( 'admin_footer', [ get_called_class(), 'print_templates' ] );
-		add_action( 'wp_footer', [ get_called_class(), 'print_templates' ] );
-
 		$attributes = static::get_attributes( $field, $meta );
 
 		$html = sprintf(
