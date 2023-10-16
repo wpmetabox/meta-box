a17202f46b28b17005984b9c5e0d06aeea006846
tuankiet2605 <tuankiet2605@gmail.com>
2023-10-16 03:09:19 +0000

    Change class inherit Select Advance

--- inc/fields/icon.php
+++ inc/fields/icon.php
@@ -4,49 +4,18 @@ defined( 'ABSPATH' ) || die;
 /**
  * The icon field.
  */
-class RWMB_Icon_Field extends RWMB_Select_Field {
+class RWMB_Icon_Field extends RWMB_Select_Advanced_Field {
 	public static function admin_enqueue_scripts() {
 		parent::admin_enqueue_scripts();
-		wp_enqueue_style( 'rwmb-select2', RWMB_CSS_URL . 'select2/select2.css', [], '4.0.10' );
-		wp_register_script( 'rwmb-select2', RWMB_JS_URL . 'select2/select2.min.js', [ 'jquery' ], '4.0.10', true );
-
-		// Localize.
-		$dependencies = [ 'rwmb-select2', 'rwmb-select', 'underscore' ];
-		$locale       = str_replace( '_', '-', get_locale() );
-		$locale_short = substr( $locale, 0, 2 );
-		$locale       = file_exists( RWMB_DIR . "js/select2/i18n/$locale.js" ) ? $locale : $locale_short;
-
-		if ( file_exists( RWMB_DIR . "js/select2/i18n/$locale.js" ) ) {
-			wp_register_script( 'rwmb-select2-i18n', RWMB_JS_URL . "select2/i18n/$locale.js", [ 'rwmb-select2' ], '4.0.10', true );
-			$dependencies[] = 'rwmb-select2-i18n';
-		}
 
 		wp_enqueue_style( 'rwmb-icon', RWMB_CSS_URL . 'icon.css', [], RWMB_VER );
-		wp_enqueue_script( 'rwmb-icon', RWMB_JS_URL . 'icon.js', $dependencies, RWMB_VER, true );
+		wp_enqueue_script( 'rwmb-icon', RWMB_JS_URL . 'icon.js', [ 'rwmb-select2', 'rwmb-select', 'underscore' ], RWMB_VER, true );
 
 		self::enqueue_style();
 	}
 
 	private static function enqueue_style() {
-		wp_enqueue_style( 'rwmb-fontawesome', RWMB_CSS_URL . 'fontawesome/fontawesome.css', [], RWMB_VER );
-	}
-
-	public static function html( $meta, $field ) {
-		$attributes                  = static::get_attributes( $field, $meta );
-		$attributes['data-selected'] = $meta;
-		$walker                      = new RWMB_Walker_Select( $field, $meta );
-
-		$output = sprintf( '<select %s>', self::render_attributes( $attributes ) );
-
-		if ( ! $field['multiple'] && $field['placeholder'] ) {
-			$output .= '<option value="">' . esc_html( $field['placeholder'] ) . '</option>';
-		}
-
-		$options = self::get_list_fonts();
-		$output .= $walker->walk( $options, $field['flatten'] ? -1 : 0 );
-		$output .= '</select>';
-		$output .= parent::get_select_all_html( $field );
-		return $output;
+		wp_enqueue_style( 'rwmb-fontawesome', RWMB_CSS_URL . 'fontawesome/all.min.css', [], RWMB_VER );
 	}
 
 	private static function get_list_fonts() {
@@ -54,16 +23,17 @@ class RWMB_Icon_Field extends RWMB_Select_Field {
 		$icon_list = [];
 
 		foreach ( $icons as $key => $icon ) {
-			$obj        = new stdClass();
-			$obj->label = $icon['label'];
+			$icon_entry = [
+				'label' => $icon['label'],
+			];
 			$icon_style = $icon['styles'];
 
-			$font_prefix = $icon_style[0] === 'brands' ? 'fab' : 'fas';
-			$obj->value  = $font_prefix . ' fa-' . $icon_style[0] . ' fa-' . $key;
+			// $font_prefix = $icon_style[0] === 'brands' ? 'fab' : 'fas';
+			$icon_entry['value'] = 'fa-' . $icon_style[0] . ' fa-' . $key;
 
-			$icon_list[ $key ] = $obj;
+			// Append the icon entry to the $icon_list array
+			$icon_list[] = $icon_entry;
 		}
-
 		return $icon_list;
 	}
 
@@ -85,29 +55,15 @@ class RWMB_Icon_Field extends RWMB_Select_Field {
 
 		$field['js_options'] = wp_parse_args( $field['js_options'], [
 			'allowClear'        => true,
-			'dropdownAutoWidth' => true,
+			'dropdownAutoWidth' => false,
 			'placeholder'       => $field['placeholder'],
 			'width'             => 'style',
+			'showIcon'          => true,
 		] );
+
+		$field['options'] = self::get_list_fonts();
 
 		return $field;
-	}
-
-	/**
-	 * Get the attributes for a field.
-	 *
-	 * @param array $field Field parameters.
-	 * @param mixed $value Meta value.
-	 * @return array
-	 */
-	public static function get_attributes( $field, $value = null ) {
-		$attributes = parent::get_attributes( $field, $value );
-		$attributes = wp_parse_args( $attributes, [
-			'value'        => $value,
-			'data-options' => wp_json_encode( $field['js_options'] ),
-		] );
-
-		return $attributes;
 	}
 
 	/**
--- js/icon.js
+++ js/icon.js
@@ -5,22 +5,8 @@
 ( function ( $, rwmb ) {
     'use strict';
 
-    function init( e ) {
-        $( e.target ).find( '.rwmb-icon' ).each( transform );
-    }
-
-    /**
-     * Transform select fields into beautiful dropdown with select2 library.
-     */
-    function transform() {
-        var $this = $( this ),
-            options = $this.data( 'options' );
-
-        $this.removeClass( 'select2-hidden-accessible' ).removeAttr( 'data-select2-id' );
-        $this.siblings( '.select2-container' ).remove();
-        $this.find( 'option' ).removeAttr( 'data-select2-id' );
-
-        $this.show().select2( {
+    function format_icon( event, options ) {
+        $( this ).select2( {
             ...options,
             templateResult: function ( option ) {
                 if ( !option.id ) {
@@ -39,19 +25,8 @@
                 return $option;
             },
         } );
-
-        /**
-         * Preserve the order that options are selected.
-         * @see https://github.com/select2/select2/issues/3106#issuecomment-255492815
-         */
-        $this.on( 'select2:select', function ( event ) {
-            var option = $this.children( '[value="' + event.params.data.id + '"]' );
-            option.detach();
-            $this.append( option ).trigger( 'change' );
-        } );
     }
 
     rwmb.$document
-        .on( 'mb_ready', init )
-        .on( 'clone', '.rwmb-icon', transform );
+        .on( 'format_icon', '.rwmb-icon', format_icon );
 } )( jQuery, rwmb );
