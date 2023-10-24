067ca8f365b4cd160c1616dfdbdad08ad5b53fbe
tuankiet2605 <tuankiet2605@gmail.com>
2023-10-24 07:31:04 +0000

    custom enqueue_script

--- tests/019-field-icon.php
+++ tests/019-field-icon.php
@@ -56,61 +56,30 @@ function field_custom_icon( $meta_boxes ) {
 	$prefix = '';
 
 	$meta_boxes[] = [
-		'title' => __('Test field icon custom'),
-		'id' => 'test_icon_custom',
+		'title'  => __( 'Test field icon custom' ),
+		'id'     => 'test_icon_custom',
 		'fields' => [
 			[
-				'name' => __('Icon custom', 'your-text-domain'),
-				'id' => $prefix . 'icon_custom_sda545',
-				'placeholder' => 'Select Icon',
-				'type' => 'icon',
-				'icon_set' => 'Line Awesome',
-				'icon_json' => RWMB_DIR . 'tests/assets/line-awesome.json',
+				'name'           => __( 'Icon custom', 'your-text-domain' ),
+				'id'             => $prefix . 'icon_custom_func',
+				'placeholder'    => 'Select Icon',
+				'type'           => 'icon',
+				'icon_set'       => 'Line Awesome',
+				'icon_json'      => RWMB_DIR . 'tests/assets/line-awesome.json',
+				// enqueue_script by function
 				'enqueue_script' => function () {
 					wp_enqueue_style( 'rwmb-custom-icon', RWMB_URL . 'tests/assets/css/line-awesome.min.css', [], RWMB_VER );
 				},
 			],
 			[
-				'name'        => __( 'Icon Clone', 'your-text-domain' ),
-				'id'          => $prefix . 'icon_custom_clone',
-				'placeholder' => 'Select Icon Clone',
-				'type'        => 'icon',
-				'icon_set' => 'Line Awesome',
-				'icon_json' => RWMB_DIR . 'tests/assets/line-awesome.json',
-				'enqueue_script' => function () {
-					wp_enqueue_style( 'rwmb-custom-icon', RWMB_URL . 'tests/assets/css/line-awesome.min.css', [], RWMB_VER );
-				},
-				'clone'       => true,
-			],
-			[
-				'name'   => __( 'Group Icon', 'your-text-domain' ),
-				'id'     => $prefix . 'group_custom_icon',
-				'type'   => 'group',
-				'fields' => [
-					[
-						'name'        => __( 'Icon in group', 'your-text-domain' ),
-						'id'          => $prefix . 'icon_custom_in_group',
-						'placeholder' => 'Select Icon',
-						'type'        => 'icon',
-						'icon_set' => 'Line Awesome',
-                        'icon_json' => RWMB_DIR . 'tests/assets/line-awesome.json',
-                        'enqueue_script' => function () {
-                            wp_enqueue_style( 'rwmb-custom-icon', RWMB_URL . 'tests/assets/css/line-awesome.min.css', [], RWMB_VER );
-                        },
-					],
-					[
-						'name'        => __( 'Icon Clone in group', 'your-text-domain' ),
-						'id'          => $prefix . 'icon_custom_clone_in_group',
-						'placeholder' => 'Select Icon',
-						'type'        => 'icon',
-						'icon_set' => 'Line Awesome',
-                        'icon_json' => RWMB_DIR . 'tests/assets/line-awesome.json',
-                        'enqueue_script' => function () {
-                            wp_enqueue_style( 'rwmb-custom-icon', RWMB_URL . 'tests/assets/css/line-awesome.min.css', [], RWMB_VER );
-                        },
-						'clone'       => true,
-					],
-				],
+				'name'           => __( 'Icon custom By Css', 'your-text-domain' ),
+				'id'             => $prefix . 'icon_custom_link_css',
+				'placeholder'    => 'Select Icon',
+				'type'           => 'icon',
+				'icon_set'       => 'Line Awesome',
+				'icon_json'      => RWMB_DIR . 'tests/assets/line-awesome.json',
+				// enqueue_script by link css
+				'enqueue_script' => RWMB_URL . 'tests/assets/css/line-awesome.min.css',
 			],
 		],
 	];
