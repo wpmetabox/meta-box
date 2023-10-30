89400fd295df1e1f831123dc5b92586ed40e56a8
Anh Tran <anhtnt@elightup.com>
2023-10-30 10:07:53 +0700

    Refactor the code

--- inc/fields/icon.php
+++ inc/fields/icon.php
@@ -13,122 +13,110 @@ class RWMB_Icon_Field extends RWMB_Select_Advanced_Field {
 
 		$args  = func_get_args();
 		$field = $args[0];
-		self::enqueue_icon_font_style( $field['enqueue_script'] );
+		self::enqueue_icon_font_style( $field );
 	}
 
-	private static function enqueue_icon_font_style( $enqueue_script = null ) {
-		if ( empty( $enqueue_script ) ) {
+	private static function enqueue_icon_font_style( array $field ) {
+		if ( $field['icon_set'] === 'fontawesome-free' ) {
 			wp_enqueue_style( 'fontawesome-free', RWMB_CSS_URL . 'fontawesome/all.min.css', [], '6.4.2' );
 			return;
 		}
 
-		if ( is_string( $enqueue_script ) ) {
-			wp_enqueue_style( 'rwmb-custom-icon', $enqueue_script, [], '6.4.2' );
+		if ( is_string( $field['icon_style'] ) ) {
+			wp_enqueue_style( $field['icon_set'], $field['icon_style'], [], RWMB_VER );
 		} else {
-			$enqueue_script();
+			$field['icon_style']();
 		}
 	}
 
 	private static function get_icons( $field ) {
 		// Get from cache to prevent reading large files.
-		$icons = wp_cache_get( 'fontawesome-icons', 'meta-box-icon-field' );
-		if ( false !== $icons || ! file_exists( RWMB_DIR . 'css/fontawesome/icons.json' ) ) {
+		$cache_key = "{$field['icon_set']}-icons";
+		$icons     = wp_cache_get( $cache_key, 'meta-box-icon-field' );
+		if ( false !== $icons ) {
 			return $icons;
 		}
 
-		$data = wp_cache_get( 'fontawesome-icons', 'meta-box-icon-field-' . $field['icon_set'] );
-		if ( false === $data ) {
-			if ( empty( $field['icon_json'] ) ) {
-				$data = json_decode( file_get_contents( RWMB_DIR . 'css/fontawesome/icons.json' ), true );
-			} else {
-				$data = file_get_contents( $field['icon_json'] );
-				if ( json_decode( $data ) ) {
-					$data = json_decode( $data, true );
-				} else {
-					$data = explode( "\n", $data );
-					$data = array_map( 'trim', $data );
-				}
-			}
-			wp_cache_set( 'fontawesome-icons', $data, 'meta-box-post-field-' . $field['icon_set'] );
+		// Get icon from a JSON or a text file.
+		$data    = file_get_contents( $field['icon_file'] );
+		$decoded = json_decode( $data, true );
+		if ( json_last_error() === JSON_ERROR_NONE ) {
+			$data = $decoded;
+		} else {
+			// Text file: each icon on a line.
+			$data = explode( "\n", $data );
+			$data = array_map( 'trim', $data );
 		}
 
+		// Reformat icons.
 		$icons = [];
 		foreach ( $data as $key => $icon ) {
-			// use icon set other
-			if ( ! empty( $field['icon_json'] ) ) {
-				// case all string
-				if ( is_string( $icon ) && is_numeric( $key ) ) {
-					$icons[ $icon ] = [
-						'label' => $icon,
-						'svg'   => '',
-					];
-					continue;
-				}
-				// case key : string
-				if ( is_string( $icon ) ) {
-					$label         = str_contains( $icon, '<svg' ) ? $key : $icon;
-					$svg           = str_contains( $icon, '<svg' ) ? $icon : '';
-					$icons[ $key ] = [
-						'label' => $label,
-						'svg'   => $svg,
-					];
-					continue;
-				}
-				// case key : object()
-				$label         = empty( $icon['label'] ) ? $key : $icon['label'];
-				$svg           = empty( $icon['svg'] ) ? '' : $icon['svg'];
-				$icons[ $key ] = [
+			// Default: FontAwesome
+			if ( $field['icon_set'] === 'fontawesome-free' ) {
+				$icons[] = [
+					'value' => "fa-{$icon['styles'][0]} fa-{$key}",
+					'label' => $icon['label'],
+					'svg'   => '',
+				];
+				continue;
+			}
+
+			// Text file: each icon on a line.
+			if ( is_string( $icon ) && is_numeric( $key ) ) {
+				$icons[] = [
+					'value' => $icon,
+					'label' => $icon,
+					'svg'   => '',
+				];
+				continue;
+			}
+
+			// JSON file: "icon-class": "Label" or "icon-class": "<svg...>".
+			if ( is_string( $icon ) ) {
+				$label   = str_contains( $icon, '<svg' ) ? $key : $icon;
+				$svg     = str_contains( $icon, '<svg' ) ? $icon : '';
+				$icons[] = [
+					'value' => $key,
 					'label' => $label,
 					'svg'   => $svg,
 				];
 				continue;
 			}
-			// fontawesome default
+
+			// JSON file: "icon-class": { "label": "Label", "svg": "<svg...>" }
+			$label   = empty( $icon['label'] ) ? $key : $icon['label'];
+			$svg     = empty( $icon['svg'] ) ? '' : $icon['svg'];
 			$icons[] = [
-				'label' => $icon['label'],
-				'value' => "fa-{$icon['styles'][0]} fa-{$key}",
+				'value' => $key,
+				'label' => $label,
+				'svg'   => $svg,
 			];
 		}
 
+		f( $icons );
+
 		// Cache the result.
-		wp_cache_set( 'fontawesome-icons', $icons, 'meta-box-post-field' );
+		wp_cache_set( $cache_key, $icons, 'meta-box-post-field' );
 		return $icons;
 	}
 
 	/**
-	 * Normalize parameters for field.
+	 * Normalize field settings.
 	 *
-	 * @param array $field Field parameters.
-	 *
+	 * @param array $field Field settings.
 	 * @return array
 	 */
 	public static function normalize( $field ) {
 		$field = wp_parse_args( $field, [
-			'list_icon'      => [],
-			'icon_set'       => 'fontawesome',
 			'placeholder'    => __( 'Select an icon', 'meta-box' ),
-			'enqueue_script' => '',
-			'icon_json'      => '',
-			'options'        => [],
+			'icon_style' => '',
+			'icon_set'       => 'fontawesome-free',
+			'icon_file'      => RWMB_DIR . 'css/fontawesome/icons.json',
 		] );
 
-		$list_icons = wp_cache_get( 'fontawesome-icons', 'meta-box-icon-field-options-' . $field['icon_set'] );
-		if (false === $list_icons) {
-			$list_icons = self::get_icons( $field );
-			wp_cache_set( 'fontawesome-icons', $list_icons, 'meta-box-post-field-options-' . $field['icon_set'] );
-		}
+		$field['options'] = self::get_icons( $field );
 
-		if ( ! empty( $list_icons ) ) {
-			foreach ( $list_icons as $k => $v ) {
-				$field['options'][] = [
-					'label' => empty( $v['label'] ) ? $k : $v['label'],
-					'value' => empty( $v['svg'] ) ? $k : $v['svg'],
-				];
-			}
-		}				
-
-		$field['list_icon'] = array_merge( $field['list_icon'], [ 'fontawesome' ] );
-		$field              = parent::normalize( $field );
+		$field = parent::normalize( $field );
 
 		return $field;
 	}
@@ -144,7 +132,7 @@ class RWMB_Icon_Field extends RWMB_Select_Advanced_Field {
 	 * @return string
 	 */
 	public static function format_value( $field, $value, $args, $post_id ) {
-		self::enqueue_icon_font_style( $field['enqueue_script'] );
+		self::enqueue_icon_font_style( $field );
 
 		return sprintf( '<i class="%s"></i>', $value );
 	}
