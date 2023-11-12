6fab2f96686f3d22e02930c2960c35811abe623e
Anh Tran <anhtnt@elightup.com>
2023-11-12 20:28:06 +0700

    Remove dashboard news

--- inc/loader.php
+++ inc/loader.php
@@ -117,12 +117,6 @@ class RWMB_Loader {
 		if ( is_admin() ) {
 			$about = new RWMB_About( $update_checker );
 			$about->init();
-
-			new RWMB_Dashboard( 'http://feeds.feedburner.com/metaboxio', 'https://metabox.io/blog/', [
-				'title'           => 'Meta Box',
-				'dismiss_tooltip' => esc_html__( 'Dismiss all Meta Box news', 'meta-box' ),
-				'dismiss_confirm' => esc_html__( 'Are you sure to dismiss all Meta Box news?', 'meta-box' ),
-			] );
 		}
 
 		// Public functions.
