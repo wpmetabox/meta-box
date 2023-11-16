1414f7c6555d9678c31e8a328f62dd9ba158a59a
Anh Tran <anhtnt@elightup.com>
2023-11-16 10:25:06 +0700

    Icon: Fix title showing SVG

--- js/icon.js
+++ js/icon.js
@@ -6,12 +6,12 @@
 	'use strict';
 
 	const template = option => {
-		// Format if label have svg
-		if ( ( option.text ).includes( '<svg' ) ) {
-			return $( `<span class="rwmb-icon-select">${ option.text }</span>` );
+		if ( option.text.includes( '<svg' ) ) {
+			const title = option.text.replace( /<svg.*?>.*?<\/svg>/, '' );
+			return $( `<span class="rwmb-icon-select" title="${ title }">${ option.text }</span>` );
 		}
 
-		return option.id ? $( `<span class="rwmb-icon-select"><i class="${ option.id }"></i>${ option.text }</span>` ) : option.text;
+		return option.id ? $( `<span class="rwmb-icon-select" title=${ option.text }><i class="${ option.id }"></i>${ option.text }</span>` ) : option.text;
 	};
 
 	function initIconField( event, options ) {
