955e6a0c11dfaf4f9d02e38a1cf08f703820c1ff
Anh Tran <anhtnt@elightup.com>
2023-11-28 15:10:36 +0700

    Fix preview posts not working in the block editor in WP 6.4. Closes #1546.

--- js/validation/validation.js
+++ js/validation/validation.js
@@ -1,4 +1,4 @@
-( function( $, rwmb, i18n ) {
+( function ( $, rwmb, i18n ) {
 	'use strict';
 
 	/**
@@ -55,12 +55,12 @@
 		}
 
 		return parts.pop();
-	}
+	};
 
 	/**
 	 * Fix validation not working for cloneable files or fields in groups.
 	 */
-	$.validator.staticRules = function( element ) {
+	$.validator.staticRules = function ( element ) {
 		let rules = {},
 			validator = $.data( element.form, "validator" );
 
@@ -120,7 +120,7 @@
 	 *
 	 * @link https://stackoverflow.com/q/931687/371240
 	 */
-	$.validator.prototype.checkForm = function() {
+	$.validator.prototype.checkForm = function () {
 		this.prepareForm();
 		for ( var i = 0, elements = ( this.currentElements = this.elements() ); elements[ i ]; i++ ) {
 			if ( this.findByName( elements[ i ].name ).length !== undefined && this.findByName( elements[ i ].name ).length > 1 ) {
@@ -146,7 +146,7 @@
 			this.$form
 				// Update underlying textarea before submit.
 				// Don't use submitHandler() because form can be submitted via Ajax on the front end.
-				.on( 'submit', function() {
+				.on( 'submit', function () {
 					if ( typeof tinyMCE !== 'undefined' ) {
 						tinyMCE.triggerSave();
 					}
@@ -155,10 +155,10 @@
 		}
 
 		showAsterisks() {
-			this.validationElements.each( function() {
+			this.validationElements.each( function () {
 				const data = $( this ).data( 'validation' );
 
-				$.each( data.rules, function( k, v ) {
+				$.each( data.rules, function ( k, v ) {
 					if ( !v[ 'required' ] ) {
 						return;
 					}
@@ -176,7 +176,7 @@
 		getSettings() {
 			this.settings = {
 				ignore: ':not(.rwmb-media,.rwmb-image_select,.rwmb-wysiwyg,.rwmb-color,.rwmb-map,.rwmb-osm,.rwmb-switch,[class|="rwmb"])',
-				errorPlacement: function( error, element ) {
+				errorPlacement: function ( error, element ) {
 					error.appendTo( element.closest( '.rwmb-input' ) );
 				},
 				errorClass: 'rwmb-error',
@@ -186,7 +186,7 @@
 
 			// Gather all validation rules.
 			var that = this;
-			this.validationElements.each( function() {
+			this.validationElements.each( function () {
 				$.extend( true, that.settings, $( this ).data( 'validation' ) );
 			} );
 		}
@@ -199,7 +199,7 @@
 			}
 			// Custom event for showing error fields inside tabs/hidden divs. Use setTimeout() to run after error class is added to inputs.
 			var that = this;
-			setTimeout( function() {
+			setTimeout( function () {
 				that.$form.trigger( 'after_validate' );
 			}, 200 );
 		}
@@ -224,16 +224,15 @@
 			}
 
 			// Change the editor method.
-			editor.savePost = function( object ) {
+			editor.savePost = function ( options = {} ) {
 				// Bypass the validation when previewing in Gutenberg.
-				if ( typeof object === 'object' && object.isPreview ) {
-					savePost( object );
-					return;
+				if ( typeof options === 'object' && options.isPreview ) {
+					return savePost( options );
 				}
 
 				// Must call savePost() here instead of in submitHandler() because the form has inline onsubmit callback.
 				if ( that.$form.valid() ) {
-					return savePost( object );
+					return savePost( options );
 				}
 			};
 		}
@@ -261,7 +260,7 @@
 
 		// Edit post, edit term, edit user, front-end form.
 		var $forms = $( '#post, #edittag, #your-profile, .rwmb-form' );
-		$forms.each( function() {
+		$forms.each( function () {
 			var form = new Validation( this );
 			form.init();
 		} );
