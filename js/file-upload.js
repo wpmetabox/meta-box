7a0a09427389669381f922fec56e59ed9a45e0e9
Anh Tran <anhtnt@elightup.com>
2024-04-06 10:13:54 +0700

    Simplify HTML

--- js/file-upload.js
+++ js/file-upload.js
@@ -7,7 +7,7 @@
 
 	FileUploadField = views.FileUploadField = MediaField.extend( {
 		createAddButton: function () {
-			this.addButton = new UploadButton( {controller: this.controller} );
+			this.addButton = new UploadButton( { controller: this.controller } );
 		}
 	} );
 
@@ -27,7 +27,7 @@
 
 			// Auto hide if you reach the max number of media
 			this.listenTo( this.controller, 'change:full', function () {
-				this.$el.toggle( ! this.controller.get( 'full' ) );
+				this.$el.toggle( !this.controller.get( 'full' ) );
 			} );
 
 			this.collection = this.controller.get( 'items' );
@@ -37,14 +37,14 @@
 				}
 				const totalFiles = parseInt( this.uploader.uploader.getOption( 'totalFiles' ) );
 				this.uploader.uploader.setOption( 'totalFiles', totalFiles - 1 );
-			} );			
+			} );
 		},
 
 		// Initializes plupload using code from wp.Uploader (wp-includes/js/plupload/wp-plupload.js)
 		initUploader: function ( $this ) {
-            var self = this,
-                $input = $this.closest( '.rwmb-input' ),
-                $process = $input.find( '.rwmb-media-view .rwmb-media-progress' ),                
+			var self = this,
+				$input = $this.closest( '.rwmb-input' ),
+				$process = $input.find( '.rwmb-media-view .rwmb-media-progress' ),
 				extensions = this.getExtensions().join( ',' ),
 				maxFileSize = this.controller.get( 'maxFileSize' ),
 				maxFiles = parseInt( this.controller.get( 'maxFiles' ) ),
@@ -53,10 +53,10 @@
 					dropzone: this.el,
 					browser: this.$( '.rwmb-browse-button' ),
 					params: {
-						post_id : $( '#post_ID' ).val()
+						post_id: $( '#post_ID' ).val()
 					},
-					added: function( attachment ) {
-						self.controller.get( 'items' ).add( [attachment] );
+					added: function ( attachment ) {
+						self.controller.get( 'items' ).add( [ attachment ] );
 					}
 				};
 
@@ -68,14 +68,14 @@
 				filters.max_file_size = maxFileSize;
 			}
 			if ( extensions ) {
-				filters.mime_types = [{title: i18nRwmbMedia.select, extensions: extensions}];
+				filters.mime_types = [ { title: i18nRwmbMedia.select, extensions: extensions } ];
 			}
 			this.uploader.uploader.setOption( 'filters', filters );
 			this.uploader.uploader.setOption( 'totalFiles', 0 );
-            
-            this.uploader.uploader.bind( 'FilesAdded', function ( up, files ) {
-				const $this = this,
-					totalFiles = parseInt( $this.getOption( 'totalFiles' ) );
+
+			this.uploader.uploader.bind( 'FilesAdded', function ( up, files ) {
+				const that = this,
+					totalFiles = parseInt( that.getOption( 'totalFiles' ) );
 
 				$.each( files, function ( i, file ) {
 					if ( maxFiles !== 0 && i >= maxFiles - totalFiles ) {
@@ -83,35 +83,32 @@
 						return;
 					}
 
-					$process.append( '<div id="' + file.id + '" class="progress"><progress value="0" max="100"></progress><span class="progress-label">' + file.name + ' - ' + file.percent + '%</span></div>' );
-					$this.setOption( 'totalFiles', parseInt( $this.getOption( 'totalFiles' ) ) + 1 );
+					$process.append( `<div id="${ file.id }" class="rwmb-progress" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+						${ file.name } - ${ file.percent } %
+					</div>` );
+					that.setOption( 'totalFiles', parseInt( that.getOption( 'totalFiles' ) ) + 1 );
 				} );
-            } );
+			} );
 
-            this.uploader.uploader.bind( 'UploadProgress', function ( up, file ) {
-				const filePercent = file.percent === 100 ? 99 : file.percent;
-				$process.find( '#' + file.id + ' progress' ).attr( 'value', filePercent );
-				$process.find( '#' + file.id + ' .progress-label' ).text( file.name + ' - ' + filePercent + '%' );
-            } );
+			const setWidth = ( file, value ) => $process.find( `#${ file.id }` ).attr( 'aria-valuenow', value ).css( '--value', value ).text( `${ file.name } - ${ value }%` );
 
-			this.uploader.uploader.bind( 'FileUploaded', function ( up, file, res ) {
-				$process.find( '#' + file.id + ' progress' ).attr( 'value', 100 );
-				$process.find( '#' + file.id + ' .progress-label' ).text( file.name + ' - 100%' );				
-                $process.find( '#' + file.id ).fadeOut( "slow" ).remove();
-            } );
+			this.uploader.uploader.bind( 'UploadProgress', ( up, file ) => setWidth( file, Math.min( file.percent, 99 ) ) );
+			this.uploader.uploader.bind( 'FileUploaded', ( up, file ) => setWidth( file, 100 ).addClass( 'rwmb-progress--completed' ).fadeOut( 'slow', function () {
+				$( this ).remove();
+			} ) );
 
-            this.uploader.uploader.bind( 'Error', function ( up, err ) {
+			this.uploader.uploader.bind( 'Error', function ( up, err ) {
 
-                if ( $input.find( '.rwmb-error' ).length === 0 ) {
-                    $input.append( '<p class="rwmb-error"></p>' );
+				if ( $input.find( '.rwmb-error' ).length === 0 ) {
+					$input.append( '<p class="rwmb-error"></p>' );
 				}
-				
+
 				const $error = $input.find( '.rwmb-error' ).empty().show();
 				$error.text( err.message );
 				setTimeout( function () {
 					$error.fadeOut( "slow" );
 				}, 5000 );
-            } );
+			} );
 
 			$this.data( 'uploader', this.uploader );
 		},
@@ -121,8 +118,8 @@
 				exts = [];
 
 			_.each( mimeTypes, function ( current, index ) {
-				if ( i18nRwmbMedia.extensions[current] ) {
-					exts = exts.concat( i18nRwmbMedia.extensions[current] );
+				if ( i18nRwmbMedia.extensions[ current ] ) {
+					exts = exts.concat( i18nRwmbMedia.extensions[ current ] );
 				}
 			} );
 			return exts;
@@ -140,8 +137,8 @@
 
 		$this.siblings( '.rwmb-media-view' ).remove();
 		$this.after( view.el );
-        // Init progress
-        view.$el.find( '.rwmb-media-list' ).after( '<div class="rwmb-media-progress"></div>' );
+		// Init progress
+		view.$el.find( '.rwmb-media-list' ).after( '<div class="rwmb-media-progress"></div>' );
 		// Init uploader after view is inserted to make wp.Uploader works.
 		view.addButton.initUploader( $this );
 
@@ -152,12 +149,12 @@
 		$( e.target ).find( '.rwmb-file_upload' ).each( initFileUpload );
 	}
 
-    function removeFile( e ) {
-        $( '.rwmb-media-progress #' + $( this ).data( 'file_id' ) ).remove();
-    }
+	function removeFile( e ) {
+		$( '.rwmb-media-progress #' + $( this ).data( 'file_id' ) ).remove();
+	}
 
-    rwmb.$document
-        .on( 'mb_ready', init )
-        .on( 'clone', '.rwmb-file_upload', initFileUpload )
-        .on( 'click', '.rwmb-file-actions .rwmb-remove-media', removeFile );
+	rwmb.$document
+		.on( 'mb_ready', init )
+		.on( 'clone', '.rwmb-file_upload', initFileUpload )
+		.on( 'click', '.rwmb-file-actions .rwmb-remove-media', removeFile );
 } )( jQuery, wp, rwmb );
