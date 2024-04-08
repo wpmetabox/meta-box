33fb8830a7f43a83c8aa62026924a8201d271417
Anh Tran <anhtnt@elightup.com>
2024-04-08 12:01:01 +0700

    Force returned value of sanitize color to string

--- inc/sanitizer.php
+++ inc/sanitizer.php
@@ -117,7 +117,7 @@ class RWMB_Sanitizer {
 		}
 
 		if ( ! str_contains( $value, 'rgb' ) ) {
-			return sanitize_hex_color( $value );
+			return (string) sanitize_hex_color( $value );
 		}
 
 		// rgba value.
