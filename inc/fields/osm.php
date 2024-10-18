8423d0ab48acf508acbe02b9745133e4b84e40ff
Anh Tran <anhtnt@elightup.com>
2024-10-18 14:55:09 +0700

    Include leaflet locally

--- inc/fields/osm.php
+++ inc/fields/osm.php
@@ -164,9 +164,7 @@ class RWMB_OSM_Field extends RWMB_Field {
 	}
 
 	private static function enqueue_map_assets() {
-		// phpcs:disable PluginCheck.CodeAnalysis.EnqueuedResourceOffloading.OffloadedContent
-		wp_enqueue_style( 'leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', [], '1.9.4' );
-		wp_enqueue_script( 'leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', [], '1.9.4', true );
-		// phpcs:enable
+		wp_enqueue_style( 'leaflet', RWMB_JS_URL . 'leaflet/leaflet.css', [], '1.9.4' );
+		wp_enqueue_script( 'leaflet', RWMB_JS_URL . 'leaflet/leaflet.js', [], '1.9.4', true );
 	}
 }
