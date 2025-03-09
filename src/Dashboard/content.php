5e2e1760a0980f35b52f2684181e73b198930524
Anh Tran <anhtnt@elightup.com>
2025-03-09 15:57:01 +0700

    Style compare header

--- src/Dashboard/content.php
+++ src/Dashboard/content.php
@@ -1,8 +1,12 @@
 <?php defined( 'ABSPATH' ) || die; ?>
 
 <header class="mb-dashboard__header">
-	<a href="https://metabox.io" target="_blank">
-		<img class="mb-dashboard__logo" src="<?php echo esc_attr( $this->assets_url ) ?>/logo.svg" alt="Meta Box" />
+	<a class="mb-dashboard__logo" href="https://metabox.io" target="_blank">
+		<svg viewBox="0 0 196 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect width="46" height="46" rx="3" fill="#000"/>
+			<path d="M8.68848 35V32.4365L10.959 31.9971V16.6895L8.68848 16.25V13.6719H10.959H17.2871L23.1025 28.54H23.1904L28.8154 13.6719H37.4287V16.25L35.1436 16.6895V31.9971L37.4287 32.4365V35H28.5957V32.4365L30.9834 31.9971V27.9102L31.0566 18.2861L30.9688 18.2715L24.5967 34.707H21.3594L14.8408 18.4033L14.7529 18.418L15.002 27.4707V31.9971L17.5215 32.4365V35H8.68848Z" fill="#fff"/>
+			<path d="M55.8203 35V32.4365L58.0908 31.9971V16.6895L55.8203 16.25V13.6719H58.0908H64.4189L70.2344 28.54H70.3223L75.9473 13.6719H84.5605V16.25L82.2754 16.6895V31.9971L84.5605 32.4365V35H75.7275V32.4365L78.1152 31.9971V27.9102L78.1885 18.2861L78.1006 18.2715L71.7285 34.707H68.4912L61.9727 18.4033L61.8848 18.418L62.1338 27.4707V31.9971L64.6533 32.4365V35H55.8203ZM93.1744 35.3076C91.6607 35.3076 90.3375 34.9756 89.2047 34.3115C88.0719 33.6377 87.193 32.7148 86.568 31.543C85.9527 30.3711 85.6451 29.0332 85.6451 27.5293V26.9434C85.6451 25.3711 85.9381 23.9795 86.524 22.7686C87.11 21.5479 87.9352 20.5908 88.9996 19.8975C90.0738 19.2041 91.3385 18.8574 92.7936 18.8574C94.2291 18.8574 95.4352 19.1406 96.4117 19.707C97.3883 20.2734 98.1305 21.0889 98.6383 22.1533C99.1461 23.2178 99.4 24.5068 99.4 26.0205V28.3496H90.0836L90.0543 28.4375C90.1129 29.1309 90.2838 29.751 90.567 30.2979C90.86 30.835 91.2701 31.2598 91.7975 31.5723C92.3248 31.875 92.9645 32.0264 93.7164 32.0264C94.4977 32.0264 95.2252 31.9287 95.899 31.7334C96.5826 31.5283 97.2467 31.2305 97.8912 30.8398L99.0484 33.4766C98.3941 34.0039 97.5689 34.4434 96.5729 34.7949C95.5865 35.1367 94.4537 35.3076 93.1744 35.3076ZM90.1129 25.5225H95.401V25.1416C95.401 24.4971 95.3131 23.9453 95.1373 23.4863C94.9713 23.0176 94.6979 22.6562 94.317 22.4023C93.9459 22.1484 93.4625 22.0215 92.8668 22.0215C92.3004 22.0215 91.8219 22.1729 91.4312 22.4756C91.0406 22.7686 90.733 23.1738 90.5084 23.6914C90.2936 24.209 90.1471 24.7949 90.0689 25.4492L90.1129 25.5225ZM107.003 35.3076C105.529 35.3076 104.391 34.9023 103.59 34.0918C102.799 33.2812 102.404 31.9971 102.404 30.2393V22.1533H100.221V19.1504H102.404V15.2686H106.666V19.1504H109.581V22.1533H106.666V30.2246C106.666 30.8398 106.793 31.2793 107.047 31.543C107.311 31.8066 107.667 31.9385 108.116 31.9385C108.361 31.9385 108.614 31.9238 108.878 31.8945C109.152 31.8555 109.386 31.8164 109.581 31.7773L109.947 34.8682C109.508 34.9951 109.029 35.0977 108.512 35.1758C108.004 35.2637 107.501 35.3076 107.003 35.3076ZM116.262 35.3076C114.689 35.3076 113.439 34.8877 112.512 34.0479C111.584 33.1982 111.12 32.0459 111.12 30.5908C111.12 29.0967 111.725 27.8857 112.936 26.958C114.147 26.0205 115.92 25.5518 118.254 25.5518H120.348V24.3799C120.348 23.6182 120.124 23.0029 119.675 22.5342C119.235 22.0557 118.571 21.8164 117.682 21.8164C117.223 21.8164 116.813 21.875 116.452 21.9922C116.091 22.0996 115.759 22.2559 115.456 22.4609L115.119 24.5557H112.043L111.984 20.4395C112.814 19.9707 113.713 19.5898 114.679 19.2969C115.646 19.0039 116.725 18.8574 117.917 18.8574C119.987 18.8574 121.623 19.3408 122.824 20.3076C124.025 21.2646 124.626 22.6367 124.626 24.4238V30.957C124.626 31.1719 124.626 31.3867 124.626 31.6016C124.636 31.8066 124.655 32.0068 124.684 32.2021L126.34 32.4365V35H121.037C120.929 34.7266 120.822 34.4336 120.715 34.1211C120.607 33.8086 120.529 33.4961 120.48 33.1836C119.972 33.8281 119.372 34.3457 118.679 34.7363C117.995 35.1172 117.189 35.3076 116.262 35.3076ZM117.404 32.1436C118 32.1436 118.566 32.0117 119.103 31.748C119.64 31.4746 120.055 31.1182 120.348 30.6787V28.1738H118.239C117.302 28.1738 116.589 28.3887 116.1 28.8184C115.622 29.248 115.383 29.7656 115.383 30.3711C115.383 30.9375 115.558 31.377 115.91 31.6895C116.271 31.9922 116.769 32.1436 117.404 32.1436ZM133.914 35V32.4365L136.185 31.9971V16.6895L133.914 16.25V13.6719H136.185H143.392C145.901 13.6719 147.859 14.1602 149.266 15.1367C150.672 16.1133 151.375 17.5732 151.375 19.5166C151.375 20.5029 151.111 21.3867 150.584 22.168C150.066 22.9395 149.31 23.5205 148.313 23.9111C149.163 24.0967 149.871 24.4238 150.438 24.8926C151.014 25.3613 151.443 25.9326 151.727 26.6064C152.01 27.2705 152.151 27.998 152.151 28.7891C152.151 30.8301 151.478 32.3779 150.13 33.4326C148.782 34.4775 146.873 35 144.402 35H133.914ZM140.462 31.7188H144.402C145.535 31.7188 146.399 31.4746 146.995 30.9863C147.591 30.4883 147.889 29.7559 147.889 28.7891C147.889 27.7441 147.635 26.9482 147.127 26.4014C146.619 25.8447 145.818 25.5664 144.725 25.5664H140.462V31.7188ZM140.462 22.6074H143.538C144.681 22.6074 145.56 22.373 146.175 21.9043C146.79 21.4355 147.098 20.752 147.098 19.8535C147.098 18.8672 146.785 18.1396 146.16 17.6709C145.545 17.2021 144.622 16.9678 143.392 16.9678H140.462V22.6074ZM160.663 35.3076C159.1 35.3076 157.757 34.9658 156.634 34.2822C155.511 33.5986 154.652 32.6514 154.056 31.4404C153.461 30.2197 153.163 28.8184 153.163 27.2363V26.9287C153.163 25.3564 153.461 23.9648 154.056 22.7539C154.652 21.5332 155.506 20.5811 156.62 19.8975C157.743 19.2041 159.081 18.8574 160.633 18.8574C162.206 18.8574 163.548 19.2041 164.662 19.8975C165.775 20.5811 166.629 21.5283 167.225 22.7393C167.821 23.9502 168.119 25.3467 168.119 26.9287V27.2363C168.119 28.8184 167.821 30.2197 167.225 31.4404C166.629 32.6514 165.775 33.5986 164.662 34.2822C163.548 34.9658 162.215 35.3076 160.663 35.3076ZM160.663 32.0264C161.405 32.0264 162.01 31.8262 162.479 31.4258C162.948 31.0254 163.295 30.4688 163.519 29.7559C163.744 29.0332 163.856 28.1934 163.856 27.2363V26.9287C163.856 25.9912 163.744 25.166 163.519 24.4531C163.295 23.7305 162.943 23.1689 162.464 22.7686C161.996 22.3584 161.385 22.1533 160.633 22.1533C159.901 22.1533 159.296 22.3584 158.817 22.7686C158.338 23.1689 157.987 23.7305 157.762 24.4531C157.547 25.166 157.44 25.9912 157.44 26.9287V27.2363C157.44 28.1934 157.547 29.0332 157.762 29.7559C157.987 30.4785 158.338 31.04 158.817 31.4404C159.296 31.8311 159.911 32.0264 160.663 32.0264ZM168.573 35V32.4365L170.61 32.1729L175.151 27.0605L170.859 21.9775L168.94 21.7285V19.1504H176.791V21.6113L175.561 21.8018L177.861 24.8193L180.16 21.8018L178.798 21.6113V19.1504H186.371V21.7285L184.599 21.9775L180.541 26.8848L185.068 32.1729L187.016 32.4365V35H178.886V32.5537L180.263 32.3633L177.817 29.3311L175.429 32.3779L176.777 32.5537V35H168.573Z" fill="#000"/>
+		</svg>
 	</a>
 
 	<input class="mb-dashboard__header__search" type="text" placeholder="<?php esc_attr_e( 'Need some help? Search here...', 'meta-box' ); ?>" />
@@ -74,16 +78,21 @@
 			</div>
 			<div class="mb-dashboard__tab-pane" data-tab="tutorials"></div>
 			<div class="mb-dashboard__tab-pane mb-dashboard__tab-pane--hidden" data-tab="videos"></div>
-			<div class="mb-dashboard__tab-pane mb-dashboard__tab-pane--hidden" data-tab="compare">
-				<h2>
-					<?php
-					// Translators: %1$s - Meta Box Lite, %2$s - Meta Box AIO.
-					echo esc_html( sprintf( __( '%1$s vs %2$s', 'meta-box' ), 'Meta Box Lite', 'Meta Box AIO' ) );
-					?>
-				</h2>
-				<p><?php esc_html_e( 'Understand what\'s the difference between the two plugins and select what best suit your needs.', 'meta-box' ); ?></p>
+			<div class="mb-dashboard__tab-pane mb-dashboard__tab-pane--hidden mb-dashboard__compare" data-tab="compare">
+				<div class="mb-dashboard__compare__header">
+					<div>
+						<div class="mb-dashboard__compare__title">
+							<?php
+							// Translators: %1$s - Meta Box Lite, %2$s - Meta Box AIO.
+							echo esc_html( sprintf( __( '%1$s vs %2$s', 'meta-box' ), 'Meta Box Lite', 'Meta Box AIO' ) );
+							?>
+						</div>
+						<p><?php esc_html_e( 'Understand what\'s the difference between the two plugins and select what best suit your needs.', 'meta-box' ); ?></p>
+					</div>
 
-				<a class="mb-dashboard__button" href="https://metabox.io/pricing/" target="_blank"><?php esc_html_e( 'Upgrade now', 'meta-box' ); ?></a>
+					<a class="mb-dashboard__button" href="https://metabox.io/pricing/" target="_blank"><?php esc_html_e( 'Upgrade now', 'meta-box' ); ?></a>
+				</div>
+
 				<table>
 					<thead>
 						<tr>
@@ -150,87 +159,87 @@
 						</tr>
 						<tr>
 							<td>Create custom fields for taxonomies</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create custom fields for users</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create custom settings pages</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create Customizer sections &amp; panels</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create user profile</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create custom content models and save their data to custom tables</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Organize custom fields into repeatable groups</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Advanced location rules for field groups</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Toogle fields or field groups with conditional logic</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Frontend submission forms and dashboard</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create templates and displaying fields with views</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Save post/term meta to custom tables</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Create custom Gutenberg blocks</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Display fields in admin columns</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>UI features: columns, tooltips, tabs</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Premium support</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 						<tr>
 							<td>Licensing management system, including sub-licensing</td>
-							<td align="center"><svg><use xlink:href="#x"></use></svg></td>
+							<td align="center"><svg><use xlink:href="#x-circle"></use></svg></td>
 							<td align="center"><svg><use xlink:href="#check-circle"></use></svg></td>
 						</tr>
 					</tbody>
@@ -339,8 +348,9 @@
 		<path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
 		<path d="M9.999 13.587 7.7 11.292l-1.412 1.416 3.713 3.705 6.706-6.706-1.414-1.414z"></path>
 	</symbol>
-	<symbol id="x" viewBox="0 0 24 24" fill="#dc2626">
-		<path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
+	<symbol id="x-circle" viewBox="0 0 24 24" fill="#dc2626">
+		<path d="M9.172 16.242 12 13.414l2.828 2.828 1.414-1.414L13.414 12l2.828-2.828-1.414-1.414L12 10.586 9.172 7.758 7.758 9.172 10.586 12l-2.828 2.828z"></path>
+		<path d="M12 22c5.514 0 10-4.486 10-10S17.514 2 12 2 2 6.486 2 12s4.486 10 10 10zm0-18c4.411 0 8 3.589 8 8s-3.589 8-8 8-8-3.589-8-8 3.589-8 8-8z"></path>
 	</symbol>
 	<symbol id="external-link" viewBox="0 0 24 24">
 		<path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
