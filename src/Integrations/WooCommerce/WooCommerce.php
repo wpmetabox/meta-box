ef7442e8c50ffbfb7a555bdc7c8bf426cdd2a9dc
LyNguyen <cnttnt@gmail.com>
2026-07-21 09:53:15 +0700

    Fix AI review

--- src/Integrations/WooCommerce/WooCommerce.php
+++ src/Integrations/WooCommerce/WooCommerce.php
@@ -16,6 +16,7 @@ class WooCommerce {
  
 	public function register() {
 		add_filter( 'rwmb_meta_box_class_name', [ $this, 'maybe_swap_meta_box_class' ], 10, 2 );
+		add_filter( 'rwmb_meta_type', [ $this, 'force_order_meta_type' ], 10, 2 );
 	}
 
 	public function maybe_swap_meta_box_class( $class_name, $settings ) {
@@ -24,6 +25,10 @@ class WooCommerce {
 		}
  
 		return 'RWMB_Order_Meta_Box';
+	}
+
+	public function force_order_meta_type( $type, $object_type ) {
+		return 'order' === $object_type ? 'shop_order' : $type;
 	}
 
 }
